dd68a45d5274292b54d23758ccfc175916604164
Kittl, Chris <chris.kittl@tu-dortmund.de>
2022-01-19 10:38:09 +0100

    Ensure unique EvcsAgent name

--- src/main/scala/edu/ie3/simona/agent/participant/evcs/EvcsAgent.scala
+++ src/main/scala/edu/ie3/simona/agent/participant/evcs/EvcsAgent.scala
@@ -8,8 +8,14 @@ package edu.ie3.simona.agent.participant.evcs
 
 import akka.actor.{ActorRef, Props}
 import edu.ie3.datamodel.models.input.system.EvcsInput
-import edu.ie3.simona.agent.participant.{ParticipantAgent, ParticipantAgentFundamentals}
-import edu.ie3.simona.agent.participant.data.Data.PrimaryData.{ApparentPower, ZERO_POWER}
+import edu.ie3.simona.agent.participant.{
+  ParticipantAgent,
+  ParticipantAgentFundamentals
+}
+import edu.ie3.simona.agent.participant.data.Data.PrimaryData.{
+  ApparentPower,
+  ZERO_POWER
+}
 import edu.ie3.simona.agent.participant.data.secondary.SecondaryDataService
 import edu.ie3.simona.agent.participant.data.secondary.SecondaryDataService.ActorEvMovementsService
 import edu.ie3.simona.agent.participant.statedata.BaseStateData.ParticipantModelBaseStateData
