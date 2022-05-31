a2a97a6c73af6ee8dc181584b035a2ab38e7617c
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2022-05-31 16:20:14 +0200

    Adding test in ResultSinkTypeSpec

--- src/test/scala/edu/ie3/simona/io/result/ResultSinkTypeSpec.scala
+++ src/test/scala/edu/ie3/simona/io/result/ResultSinkTypeSpec.scala
@@ -7,8 +7,11 @@
 package edu.ie3.simona.io.result
 
 import edu.ie3.simona.config.SimonaConfig
-import edu.ie3.simona.io.result.ResultSinkType.{Csv, InfluxDb1x}
+import edu.ie3.simona.config.SimonaConfig.ResultKafkaParams
+import edu.ie3.simona.io.result.ResultSinkType.{Csv, InfluxDb1x, Kafka}
 import edu.ie3.simona.test.common.UnitSpec
+
+import java.util.UUID
 
 class ResultSinkTypeSpec extends UnitSpec {
   "A ResultSinkType" should {
@@ -60,7 +63,39 @@ class ResultSinkTypeSpec extends UnitSpec {
       }
     }
 
-    // TODO kafka sink
+    "be instantiated correctly when supplying a kafka sink" in {
+      val conf = SimonaConfig.Simona.Output.Sink(
+        csv = None,
+        influxDb1x = None,
+        kafka = Some(
+          ResultKafkaParams(
+            "localhost:9092",
+            12,
+            "00000000-0000-0000-0000-000000000000",
+            "https://reg:123",
+            "topic"
+          )
+        )
+      )
+      val runName = "testRun"
+
+      inside(ResultSinkType(conf, runName)) {
+        case Kafka(
+              topicNodeRes,
+              runId,
+              bootstrapServers,
+              schemaRegistryUrl,
+              linger
+            ) =>
+          topicNodeRes shouldBe "topic"
+          runId shouldBe UUID.fromString("00000000-0000-0000-0000-000000000000")
+          bootstrapServers shouldBe "localhost:9092"
+          schemaRegistryUrl shouldBe "https://reg:123"
+          linger shouldBe 12
+        case _ =>
+          fail("Wrong ResultSinkType got instantiated.")
+      }
+    }
 
     "fail when more than one sink is supplied" in {
       val conf = SimonaConfig.Simona.Output.Sink(
