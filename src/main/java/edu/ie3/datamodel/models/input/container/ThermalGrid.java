3e4d16076a1a01ed08ddc66db7b23c087c4d7b36
Kittl, Chris <chris.kittl@tu-dortmund.de>
2022-07-10 13:20:10 +0200

    Replicate class from datamodel

---
+++ src/main/java/edu/ie3/datamodel/models/input/container/ThermalGrid.java
@@ -0,0 +1,54 @@
+/*
+ * © 2022. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+
+package edu.ie3.datamodel.models.input.container;
+
+import edu.ie3.datamodel.models.input.AssetInput;
+import edu.ie3.datamodel.models.input.thermal.ThermalBusInput;
+import edu.ie3.datamodel.models.input.thermal.ThermalHouseInput;
+import edu.ie3.datamodel.models.input.thermal.ThermalStorageInput;
+import java.util.*;
+
+/**
+ * Container object to denote a fully connected thermal "grid". As there are currently no branch
+ * elements, a grid always only consists of one {@link ThermalBusInput} and all its connected {@link
+ * edu.ie3.datamodel.models.input.thermal.ThermalUnitInput}s
+ *
+ * @deprecated Use this class only as long as <a
+ *     href="https://github.com/ie3-institute/PowerSystemDataModel/pull/619">PR 619</a> of
+ *     PowerSystemDataModel hasn't been completed and deployed
+ */
+public record ThermalGrid(
+    ThermalBusInput bus, Set<ThermalHouseInput> houses, Set<ThermalStorageInput> storages)
+    implements InputContainer<AssetInput> {
+  public ThermalGrid(
+      ThermalBusInput bus,
+      Collection<ThermalHouseInput> houses,
+      Collection<ThermalStorageInput> storages) {
+    this(bus, new HashSet<>(houses), new HashSet<>(storages));
+  }
+
+  @Override
+  public List<AssetInput> allEntitiesAsList() {
+    List<AssetInput> ret = new ArrayList<>(houses.size() + storages.size() + 1);
+    ret.add(bus);
+    ret.addAll(houses);
+    ret.addAll(storages);
+    return ret;
+  }
+
+  @Override
+  public String toString() {
+    return "ThermalGrid{"
+        + "bus="
+        + bus
+        + ", #houses="
+        + houses.size()
+        + ", #storages="
+        + storages.size()
+        + '}';
+  }
+}
