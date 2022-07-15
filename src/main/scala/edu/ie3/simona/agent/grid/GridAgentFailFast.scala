bf36273e687fb6323ed3dc66e0ea71a1f90a0059
Daniel Feismann <98817556+danielfeismann@users.noreply.github.com>
2022-07-15 08:04:53 +0200

    Scala Doc

--- src/main/scala/edu/ie3/simona/agent/grid/GridAgentFailFast.scala
+++ src/main/scala/edu/ie3/simona/agent/grid/GridAgentFailFast.scala
@@ -6,6 +6,7 @@
 
 package edu.ie3.simona.agent.grid
 
+import edu.ie3.datamodel.models.input.container.SubGridContainer
 import edu.ie3.simona.agent.grid.GridAgentData.GridAgentInitData
 import edu.ie3.simona.config.SimonaConfig
 import edu.ie3.simona.exceptions.agent.GridAgentInitializationException
@@ -15,6 +16,13 @@ import scala.jdk.CollectionConverters._
 
 object GridAgentFailFast {
 
+  /** FailFast Check of GridAgent at Initialisation
+    * @param gridAgentInitData
+    *   Data that is send to the [[GridAgent]] directly after startup. It
+    *   contains the main information for initialization.
+    * @param simonaConfig
+    *   the config that should be checked
+    */
   def failFast(
       gridAgentInitData: GridAgentInitData,
       simonaConfig: SimonaConfig
@@ -33,17 +41,34 @@ object GridAgentFailFast {
     /** Check if there exits voltage measurements for transformerControlGroups
       */
 
-    val maybeControlConfig: Option[SimonaConfig.Simona.Control] =
+    checkControlGroupsForMeasurement(
+      gridAgentInitData.subGridContainer,
       simonaConfig.simona.control
+    )
+  }
+
+  /** Checks all ControlGroups if a) Transformer of ControlGroup and Measurement
+    * belongs to the same sub grid. b) Measurements are measure voltage
+    * magnitude.
+    *
+    * @param subGridContainer
+    *   Container of all models for this sub grid
+    * @param maybeControlConfig
+    *   Config of ControlGroup
+    */
+  def checkControlGroupsForMeasurement(
+      subGridContainer: SubGridContainer,
+      maybeControlConfig: Option[SimonaConfig.Simona.Control]
+  ): Unit = {
 
     val measurementUnits =
-      gridAgentInitData.subGridContainer.getRawGrid.getMeasurementUnits.asScala
+      subGridContainer.getRawGrid.getMeasurementUnits.asScala
 
     val transformerUnits2W =
-      gridAgentInitData.subGridContainer.getRawGrid.getTransformer2Ws.asScala
+      subGridContainer.getRawGrid.getTransformer2Ws.asScala
 
     val transformerUnits3W =
-      gridAgentInitData.subGridContainer.getRawGrid.getTransformer3Ws.asScala
+      subGridContainer.getRawGrid.getTransformer3Ws.asScala
 
     maybeControlConfig.foreach(control =>
       control.transformer.foreach(controlGroup =>
@@ -62,18 +87,17 @@ object GridAgentFailFast {
                   .find(_.getUuid == measurement)
                   .getOrElse(
                     throw new GridAgentInitializationException(
-                      s"${gridAgentInitData.subGridContainer.getGridName} has a transformer control group (${control.transformer.toString}) with a measurement which UUID does not exists in this subnet."
+                      s"${subGridContainer.getGridName} has a transformer control group (${control.transformer.toString}) with a measurement which UUID does not exists in this subnet."
                     )
                   )
                 if (!measurementUnit.getVMag)
                   throw new GridAgentInitializationException(
-                    s"${gridAgentInitData.subGridContainer.getGridName}  has a transformer control group (${control.transformer.toString}) with a measurement which does not measure voltage magnitude."
+                    s"${subGridContainer.getGridName}  has a transformer control group (${control.transformer.toString}) with a measurement which does not measure voltage magnitude."
                   )
               }
           }
         }
       )
     )
-
   }
 }
