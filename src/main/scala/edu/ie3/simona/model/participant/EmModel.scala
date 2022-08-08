e99aab46ab126c56a9315577499b7cf9f88fa7dd
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2022-08-08 17:07:10 +0200

    Removing method copies that are probably not going to be required

--- src/main/scala/edu/ie3/simona/model/participant/EmModel.scala
+++ src/main/scala/edu/ie3/simona/model/participant/EmModel.scala
@@ -7,14 +7,11 @@
 package edu.ie3.simona.model.participant
 
 import akka.actor.ActorRef
-import edu.ie3.datamodel.exceptions.InvalidGridException
-import edu.ie3.datamodel.models.input.connector.ConnectorInput
 import edu.ie3.datamodel.models.input.system.{EmInput, SystemParticipantInput}
 import edu.ie3.simona.agent.ValueStore
 import edu.ie3.simona.agent.participant.data.Data.PrimaryData.ApparentPowerAndHeat
 import edu.ie3.simona.config.SimonaConfig.EmRuntimeConfig
 import edu.ie3.simona.model.SystemComponent
-import edu.ie3.simona.model.grid.NodeModel
 import edu.ie3.simona.model.participant.EmModel.EmRelevantData
 import edu.ie3.simona.model.participant.control.QControl
 import edu.ie3.simona.ontology.messages.FlexibilityMessage.{
@@ -108,86 +105,6 @@ object EmModel {
       uncontrolledAgents,
       controlledAgents
     )
-  }
-  // TODO:
-  /** Checks the availability of node calculation models, that are connected by
-    * the given [[ConnectorInput]]. If not both models can be found,
-    * [[InvalidGridException]] s are thrown
-    *
-    * @param connector
-    *   Connector, that connects the two queried nodes
-    * @param nodes
-    *   [[Array]] of [[NodeModel]] calculation models
-    * @return
-    *   A tuple of both connected nodes
-    */
-  private def getConnectedNodes(
-      connector: ConnectorInput,
-      nodes: Set[NodeModel]
-  ): (NodeModel, NodeModel) = {
-    val nodeAOpt: Option[NodeModel] =
-      nodes.find(_.uuid.equals(connector.getNodeA.getUuid))
-    val nodeBOpt: Option[NodeModel] =
-      nodes.find(_.uuid.equals(connector.getNodeB.getUuid))
-
-    (nodeAOpt, nodeBOpt) match {
-      case (Some(nodeA), Some(nodeB)) =>
-        (nodeA, nodeB)
-      case (None, Some(_)) =>
-        throw new InvalidGridException(
-          s"NodeA: ${connector.getNodeA.getUuid} for connector ${connector.getUuid} cannot be found."
-        )
-      case (Some(_), None) =>
-        throw new InvalidGridException(
-          s"NodeB: ${connector.getNodeB.getUuid} for connector ${connector.getUuid} cannot be found."
-        )
-      case _ =>
-        throw new InvalidGridException(
-          s"Nodes (nodeA: ${connector.getNodeA.getUuid}, nodeB: ${connector.getNodeB.getUuid})for connector ${connector.getUuid} cannot be found."
-        )
-    }
-  }
-
-  // TODO: Refactor:
-  // Questions: From where does the EmAgent know which participants it controls? -> EmScheduler
-
-  /** Checks the availability of calculation models of connected participants to
-    * the Energy Management Agent, that are connected by the given
-    * [[ConnectorInput]]. If not both models can be found,
-    * [[InvalidGridException]] s are thrown
-    *
-    * @param connector
-    *   Connector, that connects the two queried nodes
-    * @param nodes
-    *   [[Array]] of [[NodeModel]] calculation models
-    * @return
-    *   A tuple of both connected nodes
-    */
-  private def getConnectedParticipants(
-      connector: ConnectorInput,
-      nodes: Set[NodeModel]
-  ): (NodeModel, NodeModel) = {
-    val nodeAOpt: Option[NodeModel] =
-      nodes.find(_.uuid.equals(connector.getNodeA.getUuid))
-    val nodeBOpt: Option[NodeModel] =
-      nodes.find(_.uuid.equals(connector.getNodeB.getUuid))
-
-    (nodeAOpt, nodeBOpt) match {
-      case (Some(nodeA), Some(nodeB)) =>
-        (nodeA, nodeB)
-      case (None, Some(_)) =>
-        throw new InvalidGridException(
-          s"NodeA: ${connector.getNodeA.getUuid} for connector ${connector.getUuid} cannot be found."
-        )
-      case (Some(_), None) =>
-        throw new InvalidGridException(
-          s"NodeB: ${connector.getNodeB.getUuid} for connector ${connector.getUuid} cannot be found."
-        )
-      case _ =>
-        throw new InvalidGridException(
-          s"Nodes (nodeA: ${connector.getNodeA.getUuid}, nodeB: ${connector.getNodeB.getUuid})for connector ${connector.getUuid} cannot be found."
-        )
-    }
   }
 
 }
