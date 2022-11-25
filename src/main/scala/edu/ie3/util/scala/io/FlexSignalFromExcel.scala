0d14053f319c5d7ca5462aa419f0ffc3203cb083
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2022-11-25 10:20:23 +0100

    Only returning one time series type and one node in FlexSignalFromExcel

--- src/main/scala/edu/ie3/util/scala/io/FlexSignalFromExcel.scala
+++ src/main/scala/edu/ie3/util/scala/io/FlexSignalFromExcel.scala
@@ -29,82 +29,77 @@ object FlexSignalFromExcel {
   /** Gets flexibility signals from an Excel file with defined format.
     * @param filePath
     *   Path to the excel file
+    * @param nodeId
+    *   The node id, i.e. name of the sheet
+    * @param timeSeriesType
+    *   The time series type
     * @param unit
     *   Physical unit to set for values
     * @param zoneId
     *   Time zone of the incorporated date times
     * @return
-    *   A trial to a mapping from nodeId to [[TimeSeriesType]] to individual
-    *   time series
+    *   A trial to the resulting time series
     */
   def flexSignals(
       filePath: String,
+      nodeId: String,
+      timeSeriesType: TimeSeriesType.Value = TimeSeriesType.ResidualLoad,
       unit: measure.Unit[Power] = PowerSystemUnits.MEGAWATT,
       zoneId: ZoneId = ZoneId.of("UTC")
-  ): Try[
-    Map[String, Map[TimeSeriesType.Value, IndividualTimeSeries[PValue]]]
-  ] = {
+  ): Try[IndividualTimeSeries[PValue]] = {
     Using {
       val file = new File(filePath)
       val fileStream = new FileInputStream(file)
       new XSSFWorkbook(fileStream)
     } { workbook =>
-      workbook
-        .sheetIterator()
-        .asScala
-        .map { sheet =>
-          val nodeId = sheet.getSheetName
+      val sheet = workbook.getSheet(nodeId)
 
-          val rows = sheet.rowIterator().asScala
-          /* Assess the table header */
-          val header =
-            rows.next().cellIterator().asScala.toList.map(_.getStringCellValue)
-          val dateTimeIndex = header.indexOf("time")
-          val colToTimeseriesType = header.zipWithIndex
-            .filterNot(_._1 == "time")
-            .map { case (token, idx) =>
-              idx -> TimeSeriesType(token)
-            }
-            .toMap
-
-          /* Go through the file and parse the contents */
-          val values = rows.toList
-            .filterNot(_.getCell(0).getCellType == CellType.BLANK)
-            .flatMap { row =>
-              val dateTime = row
-                .getCell(dateTimeIndex)
-                .getLocalDateTimeCellValue
-                .atZone(zoneId)
-
-              val tsTypeToValue = colToTimeseriesType.map {
-                case (col, tsType) =>
-                  val raw = -row.getCell(col).getNumericCellValue
-                  val value = new PValue(
-                    Quantities.getQuantity(raw, unit)
-                  )
-                  val timeBasedValue =
-                    new TimeBasedValue[PValue](dateTime, value)
-                  tsType -> timeBasedValue
-              }
-              tsTypeToValue
-            }
-            .toList
-            .groupBy(_._1)
-            .map { case (tsType, collection) =>
-              tsType -> collection.map(_._2).toSet
-            }
-
-          /* Finally process the single value lists into individual time series */
-          val tsTypeToTs = values.map { case (tsType, valueSet) =>
-            tsType -> new IndividualTimeSeries[PValue](
-              UUID.randomUUID(),
-              valueSet.asJava
-            )
-          }
-
-          nodeId -> tsTypeToTs
+      val rows = sheet.rowIterator().asScala
+      /* Assess the table header */
+      val header =
+        rows.next().cellIterator().asScala.toList.map(_.getStringCellValue)
+      val dateTimeIndex = header.indexOf("time")
+      val colToTimeseriesType = header.zipWithIndex
+        .filterNot(_._1 == "time")
+        .map { case (token, idx) =>
+          idx -> TimeSeriesType(token)
         }
         .toMap
+
+      /* Go through the file and parse the contents */
+      val values = rows.toList
+        .filterNot(_.getCell(0).getCellType == CellType.BLANK)
+        .flatMap { row =>
+          val dateTime = row
+            .getCell(dateTimeIndex)
+            .getLocalDateTimeCellValue
+            .atZone(zoneId)
+
+          val tsTypeToValue = colToTimeseriesType.map { case (col, tsType) =>
+            val raw = -row.getCell(col).getNumericCellValue
+            val value = new PValue(
+              Quantities.getQuantity(raw, unit)
+            )
+            val timeBasedValue =
+              new TimeBasedValue[PValue](dateTime, value)
+            tsType -> timeBasedValue
+          }
+          tsTypeToValue
+        }
+        .groupBy(_._1)
+        .map { case (tsType, collection) =>
+          tsType -> collection.map(_._2).toSet
+        }
+
+      /* Finally process the single value lists into individual time series */
+      val tsTypeToTs = values.map { case (tsType, valueSet) =>
+        tsType -> new IndividualTimeSeries[PValue](
+          UUID.randomUUID(),
+          valueSet.asJava
+        )
+      }
+
+      tsTypeToTs(timeSeriesType)
     }
   }
 
