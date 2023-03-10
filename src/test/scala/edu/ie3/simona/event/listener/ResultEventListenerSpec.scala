a41cb9394fa31045009b0d8bcfc9124130e97591
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2023-03-10 16:02:37 +0100

    Revert to 60 secs

--- src/test/scala/edu/ie3/simona/event/listener/ResultEventListenerSpec.scala
+++ src/test/scala/edu/ie3/simona/event/listener/ResultEventListenerSpec.scala
@@ -71,7 +71,7 @@ class ResultEventListenerSpec
     classOf[LineResult]
   )
 
-  private val timeout = 10 minutes
+  private val timeout = 60 seconds
 
   // the OutputFileHierarchy
   private def resultFileHierarchy(
