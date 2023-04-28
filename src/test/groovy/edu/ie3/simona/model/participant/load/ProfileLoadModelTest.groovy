0aaa640f2dae841ae305926a541e6f0aed4a9a52
danielfeismann <daniel.feismann@tu-dortmund.de>
2023-04-28 12:47:29 +0200

    remove tests of scaling in ProfileLoadModelTest since scaling transfered to system participant

--- src/test/groovy/edu/ie3/simona/model/participant/load/ProfileLoadModelTest.groovy
+++ src/test/groovy/edu/ie3/simona/model/participant/load/ProfileLoadModelTest.groovy
@@ -123,45 +123,6 @@ class ProfileLoadModelTest extends Specification {
     G0      || 268.0029932985852E-6
   }
 
-  def "A profile load model should account for the (global) scaling factor correctly when scaling to maximum power within a year"() {
-    given:
-    def startDate = TimeUtil.withDefaults.toZonedDateTime("2019-01-01 00:00:00")
-    def dut = new ProfileLoadModel(
-        loadInput.uuid,
-        loadInput.id,
-        foreSeenOperationInterval,
-        globalScaling,
-        QControl.apply(loadInput.qCharacteristics),
-        loadInput.sRated,
-        loadInput.cosPhiRated,
-        H0,
-        new ActivePower(Quantities.getQuantity(268.6, WATT))
-        )
-    def relevantDatas = (0..35040).stream().map({ cnt ->
-      new ProfileLoadModel.ProfileRelevantData(
-          startDate.plus(cnt * 15, ChronoUnit.MINUTES))
-    }).collect(Collectors.toSet())
-
-    when:
-    def max = relevantDatas.stream().mapToDouble({ relevantData ->
-      dut.calculateActivePower(relevantData).to(MEGAWATT).value.doubleValue()
-    }).max().getAsDouble()
-
-    then:
-    abs(max - expectedMax) < testingTolerance
-
-    where:
-    globalScaling || expectedMax
-    0.25          || 67.00074832464630E-6
-    0.5           || 134.0014966492930E-6
-    0.75          || 201.0022449739390E-6
-    1.0           || 268.0029932985852E-6
-    1.25          || 335.0037416232310E-6
-    1.5           || 402.0044899478780E-6
-    1.75          || 469.0052382725240E-6
-    2.0           || 536.0059865971700E-6
-  }
-
   def "A profile load model should reach the targeted annual energy consumption"() {
     given:
     /* Test against a permissible deviation of 2 %. As per official documentation of the bdew load profiles
@@ -199,49 +160,5 @@ class ProfileLoadModelTest extends Specification {
     H0      || 3000d
     L0      || 3000d
     G0      || 3000d
-  }
-
-  def "A profile load model should account for the (global) scaling factor correctly when scaling to annual energy consumption"() {
-    given:
-    /* Test against a permissible deviation of 2 %. As per official documentation of the bdew load profiles
-     * [https://www.bdew.de/media/documents/2000131_Anwendung-repraesentativen_Lastprofile-Step-by-step.pdf] 1.5 %
-     * are officially permissible. But, as we currently do not take (bank) holidays into account, we cannot reach
-     * this accuracy. */
-    def testingTolerance = 0.02
-    def startDate = TimeUtil.withDefaults.toZonedDateTime("2019-01-01 00:00:00")
-    def dut = new ProfileLoadModel(
-        loadInput.uuid,
-        loadInput.id,
-        foreSeenOperationInterval,
-        globalScaling,
-        QControl.apply(loadInput.qCharacteristics),
-        loadInput.sRated,
-        loadInput.cosPhiRated,
-        H0,
-        new EnergyConsumption(Quantities.getQuantity(3000d, KILOWATTHOUR))
-        )
-    def relevantDatas = (0..35040).stream().map({ cnt ->
-      new ProfileLoadModel.ProfileRelevantData(
-          startDate.plus(cnt * 15, ChronoUnit.MINUTES))
-    }).collect(Collectors.toSet())
-
-    when:
-    def annualEnergy = relevantDatas.stream().mapToDouble({ relevantData ->
-      ((dut.calculateActivePower(relevantData) * Quantities.getQuantity(15d, MINUTE)) as ComparableQuantity<Energy>).to(KILOWATTHOUR).value.doubleValue()
-    }).sum()
-
-    then:
-    abs(annualEnergy - expectedEnergy) / expectedEnergy < testingTolerance
-
-    where:
-    globalScaling || expectedEnergy
-    0.25          || 750d
-    0.5           || 1500d
-    0.75          || 2250d
-    1.0           || 3000d
-    1.25          || 3750d
-    1.5           || 4500d
-    1.75          || 5250d
-    2.0           || 6000d
   }
 }
