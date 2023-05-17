ba7eb9002bbf1475210049d6cc2d8353c7318f99
danielfeismann <daniel.feismann@tu-dortmund.de>
2023-05-17 14:38:25 +0200

    cylindrical storage energy to volume

---
+++ src/main/scala/edu/ie3/util/scala/quantities/SquantsUtils.scala
@@ -0,0 +1,18 @@
+/*
+ * © 2023. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+
+package edu.ie3.util.scala.quantities
+
+import squants.energy.{Energy}
+import squants.space.{CubicMeters, Volume}
+
+object SquantsUtils {
+  implicit class RichEnergy(energy: Energy) {
+    def divideByEnergyDensity(that: EnergyDensity): Volume = CubicMeters(
+      energy.toWattHours / that.toWattHoursPerCubicMeter
+    )
+  }
+}
