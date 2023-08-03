a0516edee45e8dc4f905459a605843e5349f213c
Daniel Feismann <98817556+danielfeismann@users.noreply.github.com>
2023-08-03 17:17:19 +0200

    Update src/test/scala/edu/ie3/simona/model/assets/control/QControlSpec.scala
    Co-authored-by: t-ober <63147366+t-ober@users.noreply.github.com>

--- src/test/scala/edu/ie3/simona/model/assets/control/QControlSpec.scala
+++ src/test/scala/edu/ie3/simona/model/assets/control/QControlSpec.scala
@@ -118,7 +118,7 @@ class QControlSpec extends UnitSpec with TableDrivenPropertyChecks {
 
     "provide correct values when the requested value is part of the containing xy coordinates" in {
       val requestedValue = Each(0.5)
-      validCosPhiP.cosPhi(requestedValue) ~= Each(-0.8)
+      validCosPhiP.cosPhi(requestedValue) ~= Each(-0.8) shouldBe true
     }
 
     "provide an interpolated value when the requested value is not part of the containing xy coordinates" in {
