f6ab4afc3842cbfd7833a84d704b362b14881858
danielfeismann <daniel.feismann@tu-dortmund.de>
2023-08-23 12:02:19 +0200

    merge both solar altitude tests and add more test cases from reference

--- src/test/groovy/edu/ie3/simona/model/participant/PvModelTest.groovy
+++ src/test/groovy/edu/ie3/simona/model/participant/PvModelTest.groovy
@@ -247,6 +247,17 @@ class PvModelTest extends Specification {
     0d                 | -0.305432619d       | 0.518013722     || 0.7473499857948969    // omega: 0 = Solar Noon, delta: February 01st, lat/lon: Gainsville (29.68 N, 82.27 W) //Example 2.1a Goswami Priciples of Solar Engineering
     -1.374970385d      | +0.380755678d       | 0.157952297     || 0.2391202791125743d   // omega: -78.78° = 7:00 a.m., delta: June 01st, lat/lon: Tocumen Panama (9.05 N, 79.37 W) //Example 2.2a Goswami Priciples of Solar Engineering
     0d                 | -0.268780705d       | -0.616101226    || 1.2234758057948967d   // omega: 0° = Solar noon., delta: November 01st, lat/lon: Canberra Australia (35.3 S, 149.1 E) //Example 2.3b Goswami Priciples of Solar Engineering
+    43d                | -14d                | -37.5d          || 23.4529893659531784299686037109330117049955654837550  // '2011-02-13T09:30:00' from Duffie
+    43d                | 23.1d               | 97.5d           || 10.356151317506402829742934977890382350725031728508 // '2011-07-01T06:30:00' from Duffie
+    // Reference: Quaschning, Regenerative Energiesysteme figure 2.15 and figure 2.16   // gammaS@Quaschning = alphaS@SIMONA !
+    52.3d              | 23.4337425d         | -47.15114406    || 44.12595614280154d    // Berlin (13.2E 52.3N) '2011-06-21T09:00:00' MEZ
+    52.3d              | 23.4337425d         | -32.15114394d   || 52.15790489243239d    // Berlin (13.2E 52.3N) '2011-06-21T10:00:00' MEZ
+    52.3d              | 23.4337425d         | -17.15114381d   || 58.29851278388936d    // Berlin (13.2E 52.3N) '2011-06-21T11:00:00' MEZ
+    52.3d              | 23.4337425d         | -2.151143686d   || 61.086849596127955d   // Berlin (13.2E 52.3N) '2011-06-21T12:00:00' MEZ
+    52.3d              | 23.4337425d         | 12.84885587d    || 59.50792770681503d    // Berlin (13.2E 52.3N) '2011-06-21T13:00:00' MEZ
+    52.3d              | 23.4337425d         | 27.84885599d    || 54.170777340509574d   // Berlin (13.2E 52.3N) '2011-06-21T14:00:00' MEZ
+    52.3d              | 7.79402247d         | 58.28178946d    || 25.203526133755485d   // Berlin (13.2E 52.3N) '2011-09-04T16:00:00' MEZ
+    30.1d              | 23.4337425d         | 0.948855924d    || 83.28023248079408d    // Cairo  (31.3E 30.1N)  '2011-06-21T12:00:00' MEZ+1h
   }
 
   def "Calculate zenith angle thetaZ"() {
@@ -337,31 +348,6 @@ class PvModelTest extends Specification {
     -35.3d        | -17.51d         | -4.2d          | 30d        | +170d      || 14.882390116876563d                     // Goswami Priciples of Solar Engineering Example 2.7b
   }
 
-  def "Calculate the solar altitude (azimuth) angle alphaS"() {
-
-    "Calculate the solar azimuth angle for φ = 43◦ at a) 9:30 AM on February 13 and b) 6:30 PM on July 1."
-
-    "== Calculate solar altitude (azimuth) angle =="
-    given:
-    // Declination Angle delta of the sun at solar noon
-    Quantity<Angle> delta = getQuantity(Math.toRadians(deltaIn), RADIAN)
-    //Hour Angle
-    Quantity<Angle> omega = getQuantity(Math.toRadians(omegaIn), RADIAN)
-    //Latitude in Radian
-    Quantity<Angle> latitudeInRad = getQuantity(Math.toRadians(latitudeInDeg), RADIAN)
-
-    expect:
-    "- should calculate the solar altitude correctly and"
-    QuantityUtil.isEquivalentAbs(
-        pvModel.calcSolarAltitudeAngleAlphaS(omega, delta, latitudeInRad).to(DEGREE_GEOM),
-        getQuantity(alphaSOut, DEGREE_GEOM),
-        TESTING_TOLERANCE)
-
-    where: "the following parameters are given"
-    latitudeInDeg | deltaIn | omegaIn || alphaSOut
-    43d           | -14d    | -37.5d  || 23.4529893659531784299686037109330117049955654837550  // '2011-02-13T09:30:00'
-    43d           | 23.1d   | 97.5d   || 10.356151317506402829742934977890382350725031728508 // '2011-07-01T06:30:00'
-  }
 
   def "Calculate Rb (cos(thetaG)/cos(thetaZ))"() {
 
