78db07864653d53a6a1be3f97eb1d6e2819d1564
danielfeismann <daniel.feismann@tu-dortmund.de>
2023-10-20 09:47:17 +0200

    fmt

--- src/main/scala/edu/ie3/simona/agent/participant/ParticipantAgentFundamentals.scala
+++ src/main/scala/edu/ie3/simona/agent/participant/ParticipantAgentFundamentals.scala
@@ -82,8 +82,9 @@ import edu.ie3.simona.service.ServiceStateData.ServiceActivationBaseStateData
 import edu.ie3.simona.util.TickUtil._
 import edu.ie3.util.quantities.PowerSystemUnits._
 import edu.ie3.util.scala.quantities.{Megavars, QuantityUtil, ReactivePower}
-import squants.energy.{KilowattHours, Megawatts}
+import squants.energy.Megawatts
 import squants.{Dimensionless, Each, Energy, Power}
+import tech.units.indriya.quantity.Quantities
 
 import java.time.ZonedDateTime
 import java.util.UUID
