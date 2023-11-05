4ac42f76e3cb9fa3f9c100df0f39281065f14339
danielfeismann <daniel.feismann@tu-dortmund.de>
2023-11-05 15:50:30 +0100

    fmt

--- src/main/scala/edu/ie3/simona/model/participant/PvModel.scala
+++ src/main/scala/edu/ie3/simona/model/participant/PvModel.scala
@@ -11,7 +11,10 @@ import edu.ie3.simona.model.SystemComponent
 import edu.ie3.simona.model.participant.ModelState.ConstantState
 import edu.ie3.simona.model.participant.PvModel.PvRelevantData
 import edu.ie3.simona.model.participant.control.QControl
-import edu.ie3.simona.ontology.messages.FlexibilityMessage.{ProvideFlexOptions, ProvideMinMaxFlexOptions}
+import edu.ie3.simona.ontology.messages.FlexibilityMessage.{
+  ProvideFlexOptions,
+  ProvideMinMaxFlexOptions
+}
 import edu.ie3.util.quantities.PowerSystemUnits
 import edu.ie3.util.scala.OperationInterval
 import edu.ie3.util.scala.quantities._
