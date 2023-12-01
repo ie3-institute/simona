11d6966187fc13cc7fd832c64c6e8b924b8e9943
danielfeismann <daniel.feismann@tu-dortmund.de>
2023-12-01 18:26:38 +0100

    fmt

--- src/main/scala/edu/ie3/simona/model/participant/load/LoadModel.scala
+++ src/main/scala/edu/ie3/simona/model/participant/load/LoadModel.scala
@@ -11,7 +11,10 @@ import edu.ie3.datamodel.models.input.system.LoadInput
 import edu.ie3.simona.agent.participant.data.Data.PrimaryData.ApparentPower
 import edu.ie3.simona.model.participant.CalcRelevantData.LoadRelevantData
 import edu.ie3.simona.model.participant.control.QControl
-import edu.ie3.simona.model.participant.{ApparentPowerParticipant, SystemParticipant}
+import edu.ie3.simona.model.participant.{
+  ApparentPowerParticipant,
+  SystemParticipant
+}
 import edu.ie3.util.quantities.PowerSystemUnits
 import edu.ie3.util.scala.OperationInterval
 import squants.energy.Megawatts
