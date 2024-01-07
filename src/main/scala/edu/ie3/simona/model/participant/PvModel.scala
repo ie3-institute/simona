1c92a27ed9c19b3820289f95af8e6a1305c8ebd8
Simon Hütte <simon.huette@tu-dortmund.de>
2024-01-07 13:00:33 +0100

    attempt to fix error with wrong unit in the variable timeFrame

--- src/main/scala/edu/ie3/simona/model/participant/PvModel.scala
+++ src/main/scala/edu/ie3/simona/model/participant/PvModel.scala
@@ -473,7 +473,7 @@ final case class PvModel private (
 
         val omega1InRad = omega1.toRadians
         val omega2InRad = omega2.toRadians
-        val timeFrame = (omega2 - omega1) / 15d
+        val timeFrame = (omega2 - omega1).toRadians * 12 / Math.PI
 
         val a = ((sin(deltaInRad) * sin(latInRad) * cos(gammaEInRad)
           - sin(deltaInRad) * cos(latInRad) * sin(gammaEInRad) * cos(
