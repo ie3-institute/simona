86429a9d83178a297c0c7e639902ccf4ecbc2781
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2024-02-08 11:06:17 +0100

    Update src/main/scala/edu/ie3/simona/model/thermal/RandomStorageState.scala
    Fixing random stored energy state
    
    Co-authored-by: Daniel Feismann <98817556+danielfeismann@users.noreply.github.com>

--- src/main/scala/edu/ie3/simona/model/thermal/RandomStorageState.scala
+++ src/main/scala/edu/ie3/simona/model/thermal/RandomStorageState.scala
@@ -19,8 +19,7 @@ trait RandomStorageState {
   override def startingState: ThermalStorage.ThermalStorageState = {
     def rnd: Double = new Random(seed).nextDouble()
     def storedEnergy: Energy = getMinEnergyThreshold + (
-      getMaxEnergyThreshold - (getMinEnergyThreshold * rnd)
-    )
+    getMaxEnergyThreshold - getMinEnergyThreshold) * rnd
 
     ThermalStorageState(
       -1L,
