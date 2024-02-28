d641373cda86e1065f3a8c3798b83cb00462f9d3
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-02-28 14:57:38 +0100

    fmt

--- src/test/scala/edu/ie3/simona/model/grid/GridSpec.scala
+++ src/test/scala/edu/ie3/simona/model/grid/GridSpec.scala
@@ -546,10 +546,11 @@ class GridSpec
         )
         val expectedUuids = Set(node1, node2).map(_.getUuid)
 
-        val actual = TransformerControlGroupModel invokePrivate determineNodeUuids(
-          measurementUnits,
-          selectedMeasurements,
-        )
+        val actual =
+          TransformerControlGroupModel invokePrivate determineNodeUuids(
+            measurementUnits,
+            selectedMeasurements,
+          )
         actual should contain theSameElementsAs expectedUuids
       }
     }
