6fc8187572f85858770739b4d5082776879e9bb0
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2024-03-22 19:49:58 +0100

    Removing PhaseSwitch implementation and utilization

--- src/test/scala/edu/ie3/simona/sim/SimonaSimSpec.scala
+++ src/test/scala/edu/ie3/simona/sim/SimonaSimSpec.scala
@@ -60,7 +60,7 @@ class SimonaSimSpec extends ScalaTestWithActorTestKit with UnitSpec {
             ) {
               override def extSimulations(
                   context: ActorContext[_],
-                  rootScheduler: ActorRef[SchedulerMessage],
+                  scheduler: ActorRef[SchedulerMessage],
               ): ExtSimSetupData = {
                 // We cannot return a TestProbe ref here,
                 // needs to be a proper actor created by context
@@ -451,7 +451,7 @@ object SimonaSimSpec {
 
     override def extSimulations(
         context: ActorContext[_],
-        rootScheduler: ActorRef[SchedulerMessage],
+        scheduler: ActorRef[SchedulerMessage],
     ): ExtSimSetupData =
       ExtSimSetupData(Iterable.empty, Map.empty, None)
   }
