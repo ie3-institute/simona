75a5cc65c28a708ceca06a1a1fa719e849d6d941
pierrepetersmeier <pierre.petersmeier@tu-dortmund.de>
2024-04-15 12:46:15 +0200

    MarkovParamStore

--- src/main/scala/edu/ie3/simona/model/participant/load/markov/MarkovParamStore.scala
+++ src/main/scala/edu/ie3/simona/model/participant/load/markov/MarkovParamStore.scala
@@ -1,40 +1,36 @@
-import com.typesafe.scalalogging.LazyLogging
-import org.apache.commons.csv.{CSVFormat, CSVRecord}
+package edu.ie3.simona.model.participant.load.markov
 
-import java.io.{FileInputStream, InputStreamReader, Reader}
-import scala.collection.mutable
+import java.io.{InputStreamReader, Reader}
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.commons.csv.CSVFormat
+
 import scala.jdk.CollectionConverters._
 
 object MarkovParamStore extends LazyLogging {
+  def loadDefaultApplianceProbabilities(): Map[String, Double] = {
+    val reader = getDefaultReader
+    val csvParser = CSVFormat.DEFAULT
+      .withDelimiter(';')
+      .withFirstRecordAsHeader()
+      .parse(reader)
 
-  def main(args: Array[String]): Unit = {
-    val filePath = "/load/markov/probabilities/usage_probabilities.csv"
-    val data = readCSV(filePath)
-    Test(data)
-  }
-
-  def readCSV(filePath: String): Map[String, Double] = {
-    val reader: Reader = new InputStreamReader(new FileInputStream(filePath))
-    val csvFormat: CSVFormat = CSVFormat.DEFAULT.withHeader().withDelimiter(';')
-    val csvRecords: Iterable[CSVRecord] = csvFormat.parse(reader).asScala
-    val dataMap = mutable.Map[String, Double]()
-
-    for (record <- csvRecords) {
+    val records = csvParser.getRecords.asScala
+    val probabilitiesMap = records.map { record =>
       val applianceCategory = record.get("appliance_category")
-      val usageProbabilityStr = record.get("usage_probability")
-      try {
-        val usageProbability = usageProbabilityStr.toDouble
-        dataMap.put(applianceCategory, usageProbability)
-      } catch {
-        case _: NumberFormatException => logger.warn(s"Invalid usage probability format for $applianceCategory: $usageProbabilityStr")
-      }
-    }
-    dataMap.toMap
+      val usageProbability = record.get("usage_probability").toDouble
+      (applianceCategory, usageProbability)
+    }.toMap
+
+    reader.close()
+    probabilitiesMap
   }
 
-  def Test(data: Map[String, Double]): Unit = {
-    data.foreach { case (applianceCategory, usageProbability) =>
-      println(s"Appliance Category: $applianceCategory, Usage Probability: $usageProbability")
-    }
+  private def getDefaultReader: Reader = {
+    logger.info(
+      //
+    )
+    new InputStreamReader(
+      getClass.getResourceAsStream("/load/markov/probabilities/usage_probabilities/usage_probabilities.csv")
+    )
   }
 }
