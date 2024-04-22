277ed75e2ab13e6c77924012b38f76ea25ac07b6
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-04-22 18:53:37 +0200

    groupby

--- src/main/scala/edu/ie3/simona/model/participant/evcs/EvcsModel.scala
+++ src/main/scala/edu/ie3/simona/model/participant/evcs/EvcsModel.scala
@@ -294,7 +294,7 @@ final case class EvcsModel(
             )
           }
         }
-        .groupBy { case (_, entry) =>
+        .groupBy { case _ -> entry =>
           entry.tickStart
         }
         .to(SortedMap)
