61903acd48ee4716e3b0389c3f5cd4a0ceffa262
pierrepetersmeier <pierre.petersmeier@tu-dortmund.de>
2024-05-10 12:50:04 +0200

    Testfunctions for usage_probabilities and average_hh

--- src/test/scala/edu/ie3/simona/model/participant/load/markov/MarkovParamStoreSpec.scala
+++ src/test/scala/edu/ie3/simona/model/participant/load/markov/MarkovParamStoreSpec.scala
@@ -1,47 +1,20 @@
-package edu.ie3.simona.model.participant.load.markov
-
+import edu.ie3.simona.model.participant.load.markov.MarkovParamStore
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
 
-class MarkovParamStoreSpec extends AnyWordSpec with Matchers {
+class MarkovParamStoreSpec extends AnyFlatSpec with Matchers {
 
-  "MarkovParamStore" should {
-    "print Flat map" in {
-      val flatMap = MarkovParamStore.Flat()
-      println("Test Function: Flat:")
-      flatMap.foreach { case (appliance, value) =>
-        println(s"$appliance -> $value")
-      }
+    "usage_probabilities" should "return a map of appliance Category's and their corresponding probabilities" in {
+      val probabilitiesMap = MarkovParamStore.Usage_Probabilities()
+      probabilitiesMap shouldBe a[Map[_, _]]
+      probabilitiesMap.size shouldEqual 12
+      probabilitiesMap.getOrElse("other_load", 0.0) shouldEqual 1
     }
 
-    "print House map" in {
-      val houseMap = MarkovParamStore.House()
-      println("Test Function: House:")
-      houseMap.foreach { case (appliance, value) =>
-        println(s"$appliance -> $value")
-      }
-    }
-
-    "print Income map" in {
-      val incomeMap = MarkovParamStore.income()
-      println("Test Function: Income:")
-      incomeMap.foreach { case (incomeCategory, appliancesMap) =>
-        println(s"Income Category: $incomeCategory")
-        appliancesMap.foreach { case (appliance, probability) =>
-          println(s"  $appliance -> $probability")
-        }
-      }
-    }
-
-    "print Inhabitants map" in {
-      val inhabitantsMap = MarkovParamStore.inhabitants()
-      println("Test Function: Inhabitants:")
-      inhabitantsMap.foreach { case (inhabitantsCategory, appliancesMap) =>
-        println(s"Inhabitants Category: $inhabitantsCategory")
-        appliancesMap.foreach { case (appliance, probability) =>
-          println(s"  $appliance -> $probability")
-        }
-      }
+      "average_hh" should "return a map of appliances and their corresponding probabilities" in {
+        val average_HHMap = MarkovParamStore.Average_HH()
+        average_HHMap shouldBe a [Map[_, _]]
+        average_HHMap.size shouldEqual 13
+        average_HHMap.getOrElse("lighting", 0.0) shouldEqual 2.5
     }
   }
-}
