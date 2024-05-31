487140f43e041b4d3f673a1162ab116b4d15e482
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-05-31 10:20:44 +0200

    fixing some import and other issues

--- src/test/scala/edu/ie3/simona/model/participant/WecModelSpec.scala
+++ src/test/scala/edu/ie3/simona/model/participant/WecModelSpec.scala
@@ -6,37 +6,26 @@
 
 package edu.ie3.simona.model.participant
 
-import static edu.ie3.util.quantities.PowerSystemUnits.*
-import static edu.ie3.datamodel.models.StandardUnits.*
-import static edu.ie3.simona.model.participant.WecModel.WecRelevantData
-import static tech.units.indriya.quantity.Quantities.getQuantity
-
 import edu.ie3.datamodel.models.OperationTime
-import edu.ie3.datamodel.models.input.NodeInput
-import edu.ie3.datamodel.models.input.OperatorInput
 import edu.ie3.datamodel.models.input.system.WecInput
-import edu.ie3.datamodel.models.input.system.characteristic.CosPhiFixed
-import edu.ie3.datamodel.models.input.system.characteristic.WecCharacteristicInput
-import edu.ie3.datamodel.models.input.system.type.WecTypeInput
+import edu.ie3.datamodel.models.input.system.`type`.WecTypeInput
+import edu.ie3.datamodel.models.input.system.characteristic.{
+  ReactivePowerCharacteristic,
+  WecCharacteristicInput,
+}
+import edu.ie3.datamodel.models.input.{NodeInput, OperatorInput}
 import edu.ie3.datamodel.models.voltagelevels.GermanVoltageLevelUtils
+import edu.ie3.simona.test.common.{DefaultTestData, UnitSpec}
 import edu.ie3.util.TimeUtil
-import edu.ie3.util.scala.quantities.Sq
-import scala.Option
-import scala.Some
-import spock.lang.Shared
-import spock.lang.Specification
-import spock.lang.Unroll
-import squants.Each$
-import squants.motion.MetersPerSecond$
-import squants.motion.Pascals$
+import edu.ie3.util.quantities.PowerSystemUnits
+import tech.units.indriya.quantity.Quantities
+import tech.units.indriya.unit.Units.{METRE, PERCENT, SQUARE_METRE}
 
-import squants.energy.Kilowatts$
-import squants.space.SquareMeters$
-import squants.thermal.Celsius$
+import java.util.UUID
 
 class WecModelSpec extends UnitSpec with DefaultTestData {
 
-val TOLERANCE: Double = 1e-5
+  val TOLERANCE: Double = 1e-5
 
   def setupSpec(): Unit = {
     val nodeInput = new NodeInput(
@@ -44,52 +33,54 @@ val TOLERANCE: Double = 1e-5
       "NodeInput",
       OperatorInput.NO_OPERATOR_ASSIGNED,
       OperationTime.notLimited(),
-      Sq(1d),
+      Quantities.getQuantity(1, PowerSystemUnits.PU),
       false,
       NodeInput.DEFAULT_GEO_POSITION,
       GermanVoltageLevelUtils.LV,
-      -1
+      -1,
     )
 
     val typeInput = new WecTypeInput(
       UUID.randomUUID(),
       "WecTypeInput",
-      Sq(1000, Kilowatts),
-      Sq(1000, Kilowatts),
-      Sq(1200, Kilowatts),
+      Quantities.getQuantity(1000, PowerSystemUnits.EURO),
+      Quantities.getQuantity(1000, PowerSystemUnits.EURO_PER_MEGAWATTHOUR),
+      Quantities.getQuantity(1200, PowerSystemUnits.KILOWATT),
       0.95,
-      "cP:{(0.0,0.0), (1.0,0.0), (2.0,0.115933516), (3.0,0.286255595), (4.0,0.39610618), " +
-        "(5.0,0.430345211), (6.0,0.45944023), (7.0,0.479507331), (8.0,0.492113623), " +
-        "(9.0,0.500417188), (10.0,0.488466547), (11.0,0.420415054), (12.0,0.354241299), " +
-        "(13.0,0.288470591), (14.0,0.230965702), (15.0,0.18778367), (16.0,0.154728976), " +
-        "(17.0,0.128998552), (18.0,0.108671106), (19.0,0.09239975), (20.0,0.079221236), " +
-        "(21.0,0.068434282), (22.0,0.059520087), (23.0,0.052089249), (24.0,0.045845623), " +
-        "(25.0,0.040561273), (26.0,0.036058824), (27.0,0.032198846), (28.0,0.016618264), " +
-        "(29.0,0.010330976), (30.0,0.006091519), (31.0,0.003331177), (32.0,0.001641637), " +
-        "(33.0,0.000705423), (34.0,0.000196644), (35.0,0.0), (36.0,0.0), (37.0,0.0), (38.0,0.0), " +
-        "(39.0,0.0), (40.0,0.0), (41.0,0.0), (42.0,0.0), (43.0,0.0), (44.0,0.0), (45.0,0.0), " +
-        "(46.0,0.0), (47.0,0.0), (48.0,0.0), (49.0,0.0), (50.0,0.0)}"
+      // Using the characteristics of the Enercon E-82 wind turbine.
+      new WecCharacteristicInput(
+        "cP:{(0.0,0.0), (1.0,0.0), (2.0,0.115933516), (3.0,0.286255595), (4.0,0.39610618), " +
+          "(5.0,0.430345211), (6.0,0.45944023), (7.0,0.479507331), (8.0,0.492113623), " +
+          "(9.0,0.500417188), (10.0,0.488466547), (11.0,0.420415054), (12.0,0.354241299), " +
+          "(13.0,0.288470591), (14.0,0.230965702), (15.0,0.18778367), (16.0,0.154728976), " +
+          "(17.0,0.128998552), (18.0,0.108671106), (19.0,0.09239975), (20.0,0.079221236), " +
+          "(21.0,0.068434282), (22.0,0.059520087), (23.0,0.052089249), (24.0,0.045845623), " +
+          "(25.0,0.040561273), (26.0,0.036058824), (27.0,0.032198846), (28.0,0.016618264), " +
+          "(29.0,0.010330976), (30.0,0.006091519), (31.0,0.003331177), (32.0,0.001641637), " +
+          "(33.0,0.000705423), (34.0,0.000196644), (35.0,0.0), (36.0,0.0), (37.0,0.0), (38.0,0.0), " +
+          "(39.0,0.0), (40.0,0.0), (41.0,0.0), (42.0,0.0), (43.0,0.0), (44.0,0.0), (45.0,0.0), " +
+          "(46.0,0.0), (47.0,0.0), (48.0,0.0), (49.0,0.0), (50.0,0.0)}"
+      ),
+      Quantities.getQuantity(15, PERCENT),
+      Quantities.getQuantity(5281, SQUARE_METRE),
+      Quantities.getQuantity(20, METRE),
     )
 
-    val inputModel = new WecInput(
+    val wecInput = new WecInput(
       UUID.randomUUID(),
-      "WecInput",
-      new OperatorInput(UUID.randomUUID(), "NO_OPERATOR"),
-      OperationTime.notLimited(),
+      "WecTypeInput",
       nodeInput,
-      CosPhiFixed.CONSTANT_CHARACTERISTIC,
+      ReactivePowerCharacteristic.parse("cosPhiFixed:{(0.00,0.95)}"),
       null,
       typeInput,
-      false
+      false,
+    )
+
+    val wecModel = WecModel.apply(
+      wecInput,
+      1,
+      TimeUtil.withDefaults.toZonedDateTime("2020-01-01T00:00:00Z"),
+      TimeUtil.withDefaults.toZonedDateTime("2020-01-01T01:00:00Z"),
     )
   }
-
-  def buildWecModel(): WecModel = {
-    WecModel.apply(inputModel, 1,
-      TimeUtil.withDefaults.toZonedDateTime("2020-01-01T00:00:00Z"),
-      TimeUtil.withDefaults.toZonedDateTime("2020-01-01T01:00:00Z"))
-  }
-
-
-
 }
