0eed6c0b7d7c5efe41ac064f56953c0991aa1aa0
pierrepetersmeier <pierre.petersmeier@tu-dortmund.de>
2024-06-06 20:41:19 +0200

    Delete Groovy test. Implement Scala test.

--- src/test/scala/edu/ie3/simona/model/participant/WecModelSpec.scala
+++ src/test/scala/edu/ie3/simona/model/participant/WecModelSpec.scala
@@ -9,21 +9,28 @@ package edu.ie3.simona.model.participant
 import edu.ie3.datamodel.models.OperationTime
 import edu.ie3.datamodel.models.input.system.WecInput
 import edu.ie3.datamodel.models.input.system.`type`.WecTypeInput
-import edu.ie3.datamodel.models.input.system.characteristic.{
-  ReactivePowerCharacteristic,
-  WecCharacteristicInput,
-}
+import edu.ie3.datamodel.models.input.system.characteristic.{ReactivePowerCharacteristic, WecCharacteristicInput}
 import edu.ie3.datamodel.models.input.{NodeInput, OperatorInput}
 import edu.ie3.datamodel.models.voltagelevels.GermanVoltageLevelUtils
+import edu.ie3.simona.model.participant.WecModel.WecRelevantData
 import edu.ie3.simona.test.common.{DefaultTestData, UnitSpec}
 import edu.ie3.util.TimeUtil
 import edu.ie3.util.quantities.PowerSystemUnits
+import edu.ie3.util.scala.quantities.Sq
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import shapeless.syntax.std.tuple.productTupleOps
+import squants.Each
+import squants.energy.Kilowatts
+import squants.motion.{MetersPerSecond, Pascals}
+import squants.space.SquareMeters
+import squants.thermal.Celsius
 import tech.units.indriya.quantity.Quantities
 import tech.units.indriya.unit.Units.{METRE, PERCENT, SQUARE_METRE}
 
 import java.util.UUID
 
-class WecModelSpec extends UnitSpec with DefaultTestData {
+class WecModelSpec extends AnyFlatSpec with Matchers {
 
   val TOLERANCE: Double = 1e-5
 
@@ -66,7 +73,7 @@ class WecModelSpec extends UnitSpec with DefaultTestData {
       Quantities.getQuantity(20, METRE),
     )
 
-    val wecInput = new WecInput(
+    val inputModel = new WecInput(
       UUID.randomUUID(),
       "WecTypeInput",
       nodeInput,
@@ -76,11 +83,90 @@ class WecModelSpec extends UnitSpec with DefaultTestData {
       false,
     )
 
-    val wecModel = WecModel.apply(
-      wecInput,
-      1,
-      TimeUtil.withDefaults.toZonedDateTime("2020-01-01T00:00:00Z"),
-      TimeUtil.withDefaults.toZonedDateTime("2020-01-01T01:00:00Z"),
-    )
+    def buildWecModel(): WecModel = {
+      WecModel.apply(
+        inputModel,
+        1,
+        TimeUtil.withDefaults.toZonedDateTime("2020-01-01T00:00:00Z"),
+        TimeUtil.withDefaults.toZonedDateTime("2020-01-01T01:00:00Z"),
+      )
+    }
+
+    /** "WecModel" should "build correctly from input model" in { val wecModel =
+      * buildWecModel() wecModel.uuid() shouldBe inputModel.uuid wecModel.id()
+      * shouldBe inputModel.id wecModel.sRated() shouldBe
+      * Sq.create(inputModel.getType.sRated.getValue.doubleValue(), Kilowatts)
+      * wecModel.cosPhiRated() shouldBe inputModel.getType.cosPhiRated
+      * wecModel.rotorArea() shouldBe
+      * Sq.create(inputModel.getType.rotorArea.getValue.doubleValue(),
+      * SquareMeters) wecModel.betzCurve() shouldBe new
+      * WecModel.WecCharacteristic().apply(inputModel.getType.cpCharacteristic)
+      * }
+      */
+
+    "WecModel" should "calculate active power output depending on velocity" in {
+      val wecModel = buildWecModel()
+      val velocities =
+        Seq(1.0, 2.0, 3.0, 7.0, 9.0, 13.0, 15.0, 19.0, 23.0, 27.0, 34.0, 40.0)
+      val expectedPowers =
+        Seq(0, -2948.80958, -24573.41320, -522922.23257, -1140000, -1140000,
+          -1140000, -1140000, -1140000, -1140000, -24573.39638, 0)
+
+      velocities.zip(expectedPowers).foreach { case (velocity, power) =>
+        val wecData = new WecRelevantData(
+          Sq.create(velocity, MetersPerSecond),
+          Sq.create(20, Celsius),
+          Some(Sq.create(101325d, Pascals)),
+        )
+        val result =
+          wecModel.calculateActivePower(ModelState.ConstantState, wecData)
+        math.abs(result.toWatts - power) should be < TOLERANCE
+      }
+    }
+
+    "WecModel" should "calculate active power output depending on temperature" in {
+      val wecModel = buildWecModel()
+      val temperatures = Seq(35.0, 20.0, -25.0)
+      val expectedPowers = Seq(-23377.23862, -24573.41320, -29029.60338)
+
+      temperatures.zip(expectedPowers).foreach { case (temperature, power) =>
+        val wecData = new WecRelevantData(
+          Sq.create(3.0, MetersPerSecond),
+          Sq.create(temperature, Celsius),
+          Some(Sq.create(101325d, Pascals)),
+        )
+        val result =
+          wecModel.calculateActivePower(ModelState.ConstantState, wecData)
+        result.toWatts shouldEqual power +- TOLERANCE
+      }
+    }
+
+    /** "WecModel" should "determine Betz coefficient correctly" in { val
+      * wecModel = buildWecModel() val velocities = Seq(2.0, 2.5, 18.0, 27.0,
+      * 34.0, 40.0) val expectedBetzResults = Seq(0.115933516, 0.2010945555,
+      * 0.108671106, 0.032198846, 0.000196644, 0.0)
+      *
+      * velocities.zip(expectedBetzResults).foreach { case (velocity,
+      * betzResult) => val windVel = Sq.create(velocity, MetersPerSecond) val
+      * betzFactor = wecModel.determineBetzCoefficient(windVel) val expected =
+      * Sq.create(betzResult, Each) betzFactor shouldEqual expected } }
+      */
+
+    /** "WecModel" should "calculate air density correctly" in { val wecModel =
+      * buildWecModel() val testCases = Seq( ( -15.0, 100129.44, 1.35121 ), (
+      * -5.0, 99535.96, 1.29311 ), ( 0.0, 99535.96, 1.26944 ), ( 5.0, 100129.44,
+      * 1.25405 ), ( 20.0, 100129.44, 1.18988 ), ( 25.0, 100427.25, 1.17341 ), (
+      * 37.0, 100427.25, 1.12801 ), // test cases where no air pressure is given
+      * ( 0.0, -1.0, 1.2041 ), ( 5.0, -1.0, 1.2041 ), ( 40.0, -1.0, 1.2041 ) )
+      *
+      * testCases.foreach { case (temperature, pressure, densityResult) => val
+      * temperatureV = Sq.create(temperature, Celsius) val pressureV = if
+      * (pressure > 0) Some(Sq.create(pressure, Pascals)) else Option.empty
+      *
+      * val airDensity = wecModel.calculateAirDensity(temperatureV,
+      * pressureV).toKilogramsPerCubicMeter math.abs(airDensity - densityResult)
+      * should be < TOLERANCE } }
+      */
+
   }
 }
