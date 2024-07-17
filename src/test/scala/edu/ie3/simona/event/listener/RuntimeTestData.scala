df345cd34cad905eb9b551748835861cc2239a11
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2024-07-17 11:33:16 +0200

    Added ScalaDoc for test data
    Signed-off-by: Sebastian Peter <sebastian.peter@tu-dortmund.de>

--- src/test/scala/edu/ie3/simona/event/listener/RuntimeTestData.scala
+++ src/test/scala/edu/ie3/simona/event/listener/RuntimeTestData.scala
@@ -6,10 +6,16 @@
 
 package edu.ie3.simona.event.listener
 
+/** Test data used in multiple tests of [[RuntimeEventListener]]
+  */
 trait RuntimeTestData {
 
   protected val startDateTimeString: String = "2011-01-01T00:00:00Z"
   protected val endTick: Long = 3600
+
+  /** Duration in milliseconds that is used in some of
+    * [[edu.ie3.simona.event.RuntimeEvent]]
+    */
   protected val duration: Long = 10805000
   protected val errMsg: String = "testing error msg"
 
