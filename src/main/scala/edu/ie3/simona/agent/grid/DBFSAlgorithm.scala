e89e5df1965a2a5aca3a6589e3fbbd3804aff17b
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-07-17 12:02:03 +0200

    fmt

--- src/main/scala/edu/ie3/simona/agent/grid/DBFSAlgorithm.scala
+++ src/main/scala/edu/ie3/simona/agent/grid/DBFSAlgorithm.scala
@@ -16,10 +16,18 @@ import edu.ie3.powerflow.model.PowerFlowResult.FailedPowerFlowResult.FailedNewto
 import edu.ie3.powerflow.model.PowerFlowResult.SuccessFullPowerFlowResult.ValidNewtonRaphsonPFResult
 import edu.ie3.powerflow.model.enums.NodeType
 import edu.ie3.simona.agent.grid.GridAgent.idle
-import edu.ie3.simona.agent.grid.GridAgentData.{GridAgentBaseData, GridAgentConstantData, PowerFlowDoneData}
+import edu.ie3.simona.agent.grid.GridAgentData.{
+  GridAgentBaseData,
+  GridAgentConstantData,
+  PowerFlowDoneData,
+}
 import edu.ie3.simona.agent.grid.GridAgentMessages.Responses.ExchangeVoltage
 import edu.ie3.simona.agent.grid.GridAgentMessages._
-import edu.ie3.simona.agent.participant.ParticipantAgent.{FinishParticipantSimulation, ParticipantMessage, RequestAssetPowerMessage}
+import edu.ie3.simona.agent.participant.ParticipantAgent.{
+  FinishParticipantSimulation,
+  ParticipantMessage,
+  RequestAssetPowerMessage,
+}
 import edu.ie3.simona.event.RuntimeEvent.PowerFlowFailed
 import edu.ie3.simona.exceptions.agent.DBFSAlgorithmException
 import edu.ie3.simona.model.grid.{NodeModel, RefSystem}
@@ -30,7 +38,11 @@ import edu.ie3.util.scala.quantities.DefaultQuantities._
 import edu.ie3.util.scala.quantities.SquantsUtils.RichElectricPotential
 import org.apache.pekko.actor.typed.scaladsl.AskPattern._
 import org.apache.pekko.actor.typed.scaladsl.adapter.TypedActorRefOps
-import org.apache.pekko.actor.typed.scaladsl.{ActorContext, Behaviors, StashBuffer}
+import org.apache.pekko.actor.typed.scaladsl.{
+  ActorContext,
+  Behaviors,
+  StashBuffer,
+}
 import org.apache.pekko.actor.typed.{ActorRef, Behavior, Scheduler}
 import org.apache.pekko.pattern.ask
 import org.apache.pekko.util.{Timeout => PekkoTimeout}
