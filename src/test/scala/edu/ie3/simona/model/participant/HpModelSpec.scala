fd31d9c50dc9a7cb3cb0d112afde5c5fdd41852d
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-08-16 08:14:22 +0200

    Revert "rollback unnecessary change in HpModelSpec"
    This reverts commit c6e79c78a55bee6d8067f61f319b2d1da6567356.

--- src/test/scala/edu/ie3/simona/model/participant/HpModelSpec.scala
+++ src/test/scala/edu/ie3/simona/model/participant/HpModelSpec.scala
@@ -252,7 +252,7 @@ class HpModelSpec
             Some(
               ThermalStorageState(
                 0L,
-                KilowattHours(20),
+                KilowattHours(250),
                 Kilowatts(0),
               )
             ),
