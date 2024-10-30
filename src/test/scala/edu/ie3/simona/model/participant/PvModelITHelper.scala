17fdd6de669f706e501f24236552aa0e82e5f843
pierrepetersmeier <pierre.petersmeier@tu-dortmund.de>
2024-10-30 17:50:04 +0100

    Daniel Feismann -Meeting

--- src/test/scala/edu/ie3/simona/model/participant/PvModelITHelper.scala
+++ src/test/scala/edu/ie3/simona/model/participant/PvModelITHelper.scala
@@ -6,35 +6,31 @@
 
 package edu.ie3.simona.model.participant
 
-import edu.ie3.datamodel.models.input.OperatorInput
+import edu.ie3.datamodel.models.input.{NodeInput, OperatorInput}
 import edu.ie3.datamodel.models.input.system.PvInput
 import edu.ie3.datamodel.models.input.system.characteristic.CosPhiFixed
+import edu.ie3.datamodel.models.voltagelevels.GermanVoltageLevelUtils
 import edu.ie3.datamodel.models.{OperationTime, StandardUnits}
 import edu.ie3.simona.ontology.messages.services.WeatherMessage
 import edu.ie3.util.TimeUtil
+import edu.ie3.util.quantities.PowerSystemUnits.PU
 import edu.ie3.util.scala.quantities.WattsPerSquareMeter
 import org.apache.commons.csv.{CSVFormat, CSVRecord}
-import org.apache.kafka.common.serialization.Serdes.UUID
 import squants.energy.Megawatts
 import squants.motion.MetersPerSecond
 import squants.space.Degrees
 import squants.{Angle, Kelvin, Power}
 import tech.units.indriya.quantity.Quantities
+import tech.units.indriya.quantity.Quantities.getQuantity
 
-import java.io.{BufferedReader, File, FileInputStream, InputStreamReader}
+import java.io.{BufferedReader, File, FileInputStream, InputStreamReader, Reader}
 import java.nio.file.{Files, Path}
 import java.time.ZonedDateTime
-import java.util.zip.GZIPInputStream
-import scala.collection.convert.ImplicitConversions.`iterator asScala`
-import scala.collection.mutable
-import scala.jdk.CollectionConverters.CollectionHasAsScala
-import scala.reflect.runtime.universe.Try
 import java.util.UUID
-import edu.ie3.datamodel.models.input.NodeInput
-import edu.ie3.datamodel.models.voltagelevels.GermanVoltageLevelUtils
-import edu.ie3.simona.agent.participant.data.Data.PrimaryData.ZERO_POWER.p
-import edu.ie3.util.quantities.PowerSystemUnits.PU
-import tech.units.indriya.quantity.Quantities.getQuantity
+import java.util.zip.GZIPInputStream
+import scala.collection.convert.ImplicitConversions.{`iterator asScala`, `map AsJavaMap`}
+import scala.collection.immutable.HashMap
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 trait PvModelITHelper {
 
@@ -58,44 +54,40 @@ trait PvModelITHelper {
       OperationTime.notLimited(),
       getQuantity(1, PU),
       false,
-      p,
+      NodeInput.DEFAULT_GEO_POSITION,
       GermanVoltageLevelUtils.MV_20KV,
       11,
     )
 
     val fields = line.split(";")
     if (fields.length >= 15) {
-      Try {
-        new PvInput(
-          UUID.fromString(fields(0)),
-          fields(6),
-          new OperatorInput(UUID.randomUUID(), "NO_OPERATOR"),
-          OperationTime.notLimited(),
-          nodeInput,
-          CosPhiFixed.CONSTANT_CHARACTERISTIC,
-          null,
-          fields(8).toInt,
-          Quantities.getQuantity(fields(2).toDouble, StandardUnits.AZIMUTH),
-          Quantities.getQuantity(fields(4).toDouble, StandardUnits.EFFICIENCY),
-          Quantities.getQuantity(
-            fields(5).toDouble,
-            StandardUnits.SOLAR_ELEVATION_ANGLE,
-          ),
-          fields(12).toDouble,
-          fields(7).toDouble,
-          fields(9).toBoolean,
-          Quantities.getQuantity(fields(12).toDouble, StandardUnits.S_RATED),
-          fields(3).toDouble,
-        )
-      }.toOption
-    } else {
-      None
-    }
+      Some(new PvInput(
+        UUID.fromString(fields(0)),
+        fields(6),
+        new OperatorInput(UUID.randomUUID(), "NO_OPERATOR"),
+        OperationTime.notLimited(),
+        nodeInput,
+        CosPhiFixed.CONSTANT_CHARACTERISTIC,
+        null,
+        fields(8).toInt,
+        Quantities.getQuantity(fields(2).toDouble, StandardUnits.AZIMUTH),
+        Quantities.getQuantity(fields(4).toDouble, StandardUnits.EFFICIENCY),
+        Quantities.getQuantity(
+          fields(5).toDouble,
+          StandardUnits.SOLAR_ELEVATION_ANGLE,
+        ),
+        fields(12).toDouble,
+        fields(7).toDouble,
+        fields(9).toBoolean,
+        Quantities.getQuantity(fields(12).toDouble, StandardUnits.S_RATED),
+        fields(3).toDouble,
+      ))
+    } else throw new RuntimeException("This should...")
   }
 
   def getCreatePvModels: Map[String, PvModel] = {
 
-    val csvPath = Path.of(getClass.getResource("_pv/it/grid_data.csv").toURI)
+    val csvPath = Path.of(getClass.getResource("_pv.it/grid_data").toURI)
     val lines = Files.readAllLines(csvPath).asScala
     val pvPlants: Set[PvInput] = lines.drop(1).flatMap(parsePvInputCSV).toSet
     val simulationStartDate =
@@ -103,7 +95,7 @@ trait PvModelITHelper {
     val simulationEndDate =
       TimeUtil.withDefaults.toZonedDateTime("2012-01-01T00:00:00Z")
 
-    val pvModels = mutable.Map[String, PvModel]()
+    val pvModels : HashMap[String, PvModel] = HashMap.empty
 
     pvPlants.foreach { inputModel =>
       val model = PvModel(
@@ -112,13 +104,13 @@ trait PvModelITHelper {
         simulationStartDate,
         simulationEndDate,
       )
-      pvModels.put(inputModel.id, model)
+      pvModels.put(inputModel.getId, model)
     }
-    pvModels.toMap
+    pvModels
   }
 
-  def getWeatherData
-      : Map[ZonedDateTime, Map[String, WeatherMessage.WeatherData]] = {
+
+  def getWeatherData : Map[ZonedDateTime, Map[String, WeatherMessage.WeatherData]] = {
     val filePath = Path.of("resources/_pv/it/weather.tar.gz")
     val csvRecords = getCsvRecords(filePath.toString)
 
