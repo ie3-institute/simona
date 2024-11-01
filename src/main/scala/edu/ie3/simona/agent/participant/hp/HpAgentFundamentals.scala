f44dddff0798a43571e0440edeb78ef265b46284
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-11-01 18:15:07 +0100

    merging dev

--- src/main/scala/edu/ie3/simona/agent/participant/hp/HpAgentFundamentals.scala
+++ src/main/scala/edu/ie3/simona/agent/participant/hp/HpAgentFundamentals.scala
@@ -189,7 +189,7 @@ trait HpAgentFundamentals
 
     val accompanyingResults = baseStateData.model.thermalGrid.results(
       updatedState.tick,
-      updatedState.thermalGridState,
+      updatedState.currentThermalGridState,
     )(baseStateData.startDate)
     val result = AccompaniedSimulationResult(power, accompanyingResults)
 
@@ -254,7 +254,7 @@ trait HpAgentFundamentals
     )
     val accompanyingResults = baseStateData.model.thermalGrid.results(
       updatedState.tick,
-      updatedState.thermalGridState,
+      updatedState.currentThermalGridState,
     )(baseStateData.startDate)
     val result = AccompaniedSimulationResult(power, accompanyingResults)
 
