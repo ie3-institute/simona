06baf0526f1be3896cf72e3d6ae86107da5972cc
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-11-15 08:56:37 +0100

    more grammar

--- src/main/scala/edu/ie3/simona/util/CollectionUtils.scala
+++ src/main/scala/edu/ie3/simona/util/CollectionUtils.scala
@@ -98,7 +98,7 @@ object CollectionUtils {
     * smallest map key is provided.
     *
     * @param map
-    *   containing containing the (k,v) pairs (e.g. x,y pairs)
+    *   containing the (k,v) pairs (e.g. x,y pairs)
     * @param key
     *   the key values are needed for
     * @return
