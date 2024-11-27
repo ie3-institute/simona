f76ab35265aa94503e92cd037e66d55c074aba68
Johannes Bao <smjobaoo@ie3.e-technik.tu-dortmund.de>
2024-11-27 12:30:16 +0100

    Introduce simopsim

--- src/main/scala/edu/ie3/simona/sim/setup/SimonaMosaikSetup.scala
+++ src/main/scala/edu/ie3/simona/sim/setup/SimonaMosaikSetup.scala
@@ -12,7 +12,6 @@ import edu.ie3.simona.config.SimonaConfig
 import edu.ie3.simona.event.RuntimeEvent
 import edu.ie3.simona.ontology.messages.SchedulerMessage
 import edu.ie3.simona.util.ResultFileHierarchy
-import edu.ie3.simosaik.simosaikElectrolyzer.MosaikElectrolyzerSimulation
 import org.apache.pekko.actor.typed.ActorRef
 import org.apache.pekko.actor.typed.scaladsl.ActorContext
 
@@ -46,12 +45,12 @@ class SimonaMosaikSetup(
                              ): ExtSimSetupData = {
     val mosaikAddress = mosaikIP.getOrElse("127.0.0.1:5678")
     val mosaikMapping = mosaikMappingPath.getOrElse(throw new RuntimeException("Cannot connect to Mosaik, because there is no mapping!"))
-    val mosaikExtSim = new MosaikElectrolyzerSimulation(mosaikAddress, Path.of(mosaikMapping))
+    //val mosaikExtSim = new MosaikElectrolyzerSimulation(mosaikAddress, Path.of(mosaikMapping))
 
     extSimulationSetup(
       context,
       scheduler,
-      mosaikExtSim
+      null
     )
   }
 }
--- src/main/scala/edu/ie3/simona/sim/setup/SimonaSimpleExtSimulationSetup.scala
+++ src/main/scala/edu/ie3/simona/sim/setup/SimonaSimpleExtSimulationSetup.scala
@@ -8,43 +8,14 @@ package edu.ie3.simona.sim.setup
 
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
-import edu.ie3.datamodel.graph.SubGridTopologyGraph
-import edu.ie3.datamodel.models.input.container.{GridContainer, ThermalGrid}
-import edu.ie3.datamodel.models.input.thermal.ThermalBusInput
-import edu.ie3.simona.actor.SimonaActorNaming.RichActorRefFactory
-import edu.ie3.simona.agent.EnvironmentRefs
-import edu.ie3.simona.agent.grid.GridAgent
-import edu.ie3.simona.agent.grid.GridAgentMessages.CreateGridAgent
-import edu.ie3.simona.config.{ArgsParser, RefSystemParser, SimonaConfig}
-import edu.ie3.simona.event.listener.{ResultEventListener, RuntimeEventListener}
-import edu.ie3.simona.event.{ResultEvent, RuntimeEvent}
-import edu.ie3.simona.exceptions.agent.GridAgentInitializationException
-import edu.ie3.simona.io.grid.GridProvider
+import edu.ie3.simona.config.SimonaConfig
+import edu.ie3.simona.event.RuntimeEvent
 import edu.ie3.simona.ontology.messages.SchedulerMessage
-import edu.ie3.simona.ontology.messages.SchedulerMessage.ScheduleActivation
-import edu.ie3.simona.scheduler.core.Core.CoreFactory
-import edu.ie3.simona.scheduler.core.RegularSchedulerCore
-import edu.ie3.simona.scheduler.{ScheduleLock, Scheduler, TimeAdvancer}
-import edu.ie3.simona.service.SimonaService
-import edu.ie3.simona.service.primary.PrimaryServiceProxy
-import edu.ie3.simona.service.primary.PrimaryServiceProxy.InitPrimaryServiceProxyStateData
-import edu.ie3.simona.service.results.ExtResultDataProvider
-import edu.ie3.simona.service.weather.WeatherService
-import edu.ie3.simona.service.weather.WeatherService.InitWeatherServiceStateData
-import edu.ie3.simona.sim.SimonaSim
 import edu.ie3.simona.util.ResultFileHierarchy
-import edu.ie3.simona.util.SimonaConstants.INIT_SIM_TICK
-import edu.ie3.simona.util.TickUtil.RichZonedDateTime
-import edu.ie3.simpleextsim.{SimpleExtSimulationWithEm, SimpleExtSimulationWithPowerFlow}
-import edu.ie3.util.TimeUtil
 import org.apache.pekko.actor.typed.ActorRef
 import org.apache.pekko.actor.typed.scaladsl.ActorContext
-import org.apache.pekko.actor.typed.scaladsl.adapter.{ClassicActorRefOps, TypedActorContextOps, TypedActorRefOps}
-import org.apache.pekko.actor.{ActorRef => ClassicRef}
 
-import java.util.UUID
 import java.util.concurrent.LinkedBlockingQueue
-import scala.jdk.CollectionConverters._
 
 /** Sample implementation to run a standalone simulation of simona configured
   * with the provided [[SimonaConfig]] and [[ResultFileHierarchy]]
@@ -70,14 +41,14 @@ class SimonaSimpleExtSimulationSetup(
                                context: ActorContext[_],
                                scheduler: ActorRef[SchedulerMessage],
                              ): ExtSimSetupData = {
-    val simpleExtSim = new SimpleExtSimulationWithPowerFlow()
+    //val simpleExtSim = new SimpleExtSimulationWithPowerFlow()
     //val simpleExtSim = new SimpleExtSimulationWithEm()
     //val simpleExtSim = new SimpleExtSimulationWithPrimaryData()
 
     extSimulationSetup(
       context,
       scheduler,
-      simpleExtSim
+      null
     )
   }
 }
