bcbad0e3339824d417b78ad7ab163017a3259a7c
Pierre <155652256+pierrepetersmeier@users.noreply.github.com>
2025-01-29 16:11:51 +0100

    Update src/test/scala/edu/ie3/simona/test/common/input/PvInputTestData.scala
    Co-authored-by: Daniel Feismann <98817556+danielfeismann@users.noreply.github.com>

--- src/test/scala/edu/ie3/simona/test/common/input/PvInputTestData.scala
+++ src/test/scala/edu/ie3/simona/test/common/input/PvInputTestData.scala
@@ -58,7 +58,7 @@ trait PvInputTestData
   protected val pvSouth1 = new PvInput(
     UUID.fromString("7ac5bb15-36ee-42b0-902b-9cd520e241b3"),
     "pv_south_1",
-    new OperatorInput(UUID.randomUUID(), "NO_OPERATOR"),
+OperatorInput.NO_OPERATOR_ASSIGNED,
     OperationTime.notLimited,
     nodeInputPvModel3,
     CosPhiFixed.CONSTANT_CHARACTERISTIC,
