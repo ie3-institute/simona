39c86f505dc1eda4b5d90e22a50a5b34b5df99ef
pierrepetersmeier <155652256+pierrepetersmeier@users.noreply.github.com>
2025-02-09 14:58:39 +0100

    Improve logging for missing direct solar irradiance data in WeatherSourceWrapper at night.

--- src/main/scala/edu/ie3/simona/service/weather/WeatherSourceWrapper.scala
+++ src/main/scala/edu/ie3/simona/service/weather/WeatherSourceWrapper.scala
@@ -7,21 +7,39 @@
 package edu.ie3.simona.service.weather
 
 import com.typesafe.scalalogging.LazyLogging
-import edu.ie3.datamodel.io.connectors.{CouchbaseConnector, InfluxDbConnector, SqlConnector}
-import edu.ie3.datamodel.io.factory.timeseries.{CosmoTimeBasedWeatherValueFactory, IconTimeBasedWeatherValueFactory}
+import edu.ie3.datamodel.io.connectors.{
+  CouchbaseConnector,
+  InfluxDbConnector,
+  SqlConnector,
+}
+import edu.ie3.datamodel.io.factory.timeseries.{
+  CosmoTimeBasedWeatherValueFactory,
+  IconTimeBasedWeatherValueFactory,
+}
 import edu.ie3.datamodel.io.naming.FileNamingStrategy
 import edu.ie3.datamodel.io.source.couchbase.CouchbaseWeatherSource
 import edu.ie3.datamodel.io.source.csv.CsvWeatherSource
 import edu.ie3.datamodel.io.source.influxdb.InfluxDbWeatherSource
 import edu.ie3.datamodel.io.source.sql.SqlWeatherSource
-import edu.ie3.datamodel.io.source.{IdCoordinateSource, WeatherSource => PsdmWeatherSource}
+import edu.ie3.datamodel.io.source.{
+  IdCoordinateSource,
+  WeatherSource => PsdmWeatherSource,
+}
 import edu.ie3.simona.config.SimonaConfig
 import edu.ie3.simona.config.SimonaConfig.BaseCsvParams
-import edu.ie3.simona.config.SimonaConfig.Simona.Input.Weather.Datasource.{CouchbaseParams, InfluxDb1xParams, SqlParams}
+import edu.ie3.simona.config.SimonaConfig.Simona.Input.Weather.Datasource.{
+  CouchbaseParams,
+  InfluxDb1xParams,
+  SqlParams,
+}
 import edu.ie3.simona.exceptions.InitializationException
 import edu.ie3.simona.ontology.messages.services.WeatherMessage
 import edu.ie3.simona.ontology.messages.services.WeatherMessage.WeatherData
-import edu.ie3.simona.service.weather.WeatherSource.{EMPTY_WEATHER_DATA, WeatherScheme, toWeatherData}
+import edu.ie3.simona.service.weather.WeatherSource.{
+  EMPTY_WEATHER_DATA,
+  WeatherScheme,
+  toWeatherData,
+}
 import edu.ie3.simona.service.weather.WeatherSourceWrapper.WeightSum
 import edu.ie3.simona.service.weather.{WeatherSource => SimonaWeatherSource}
 import edu.ie3.simona.util.TickUtil
@@ -125,9 +143,11 @@ private[weather] final case class WeatherSourceWrapper private (
         }
 
         val (dirIrradiance, dirIrrWeight) = currentWeather.dirIrr match {
-          case EMPTY_WEATHER_DATA.`dirIrr` =>
-            logger.warn(s"Direct solar irradiance not available at $point.")
+          case _ if currentWeather.dirIrr eq EMPTY_WEATHER_DATA.dirIrr =>
+            logger.warn(s"Direct solar irradiance data is missing at $point.")
             (averagedWeather.dirIrr, 0d)
+          case WattsPerSquareMeter(0d) =>
+            (averagedWeather.dirIrr, weight)
           case nonEmptyDirIrr =>
             (averagedWeather.dirIrr + nonEmptyDirIrr * weight, weight)
         }
