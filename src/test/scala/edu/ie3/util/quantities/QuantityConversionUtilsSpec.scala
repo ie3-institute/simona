6cc677edf14a43919906cc66eacdfc3643ca227f
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-05-22 12:53:00 +0200

    convert to should, in testing format

--- src/test/scala/edu/ie3/util/quantities/QuantityConversionUtilsSpec.scala
+++ src/test/scala/edu/ie3/util/quantities/QuantityConversionUtilsSpec.scala
@@ -6,40 +6,36 @@
 
 package edu.ie3.util.quantities
 
-import edu.ie3.simona.test.matchers.SquantsMatchers
+import edu.ie3.simona.test.common.UnitSpec
 import edu.ie3.util.DoubleUtils.~=
 import edu.ie3.util.scala.quantities.QuantityConversionUtils.TemperatureConversionSimona
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-import squants.thermal.Celsius
+import squants.thermal.{Celsius, Temperature}
 import tech.units.indriya.quantity.Quantities
 import tech.units.indriya.unit.Units.{CELSIUS, KELVIN}
 
-class QuantityConversionUtilsSpec
-    extends AnyFlatSpec
-    with Matchers
-    with SquantsMatchers {
+class QuantityConversionUtilsSpec extends UnitSpec {
   implicit val doubleTolerance: Double = 1e-10
+  implicit val temperatureTolerance: Temperature = Celsius(1e-10)
 
-  behavior of "QuantityConversionUtils"
+  "QuantityConversionUtils" should {
+    "properly convert from ComparableQuantity[Celsius] to squants temperatures and its double values" in {
+      val temperatureQuantityCelsius = Quantities.getQuantity(10, CELSIUS)
 
-  it should "properly convert from ComparableQuantity[Celsius] to squants temperatures and its double values" in {
-    val temperatureQuantityCelsius = Quantities.getQuantity(10, CELSIUS)
+      temperatureQuantityCelsius.toSquants shouldBe Celsius(10)
+      temperatureQuantityCelsius.toSquants.value shouldBe 10.0
+      temperatureQuantityCelsius.toSquants.toCelsiusDegrees shouldBe 10.0
+      temperatureQuantityCelsius.toSquants.toCelsiusScale shouldBe 10.0
+      temperatureQuantityCelsius.toSquants.toKelvinDegrees shouldBe 10.0
+      temperatureQuantityCelsius.toSquants.toKelvinScale shouldBe 283.15
 
-    temperatureQuantityCelsius.toSquants shouldBe Celsius(10)
-    temperatureQuantityCelsius.toSquants.value shouldBe 10.0
-    temperatureQuantityCelsius.toSquants.toCelsiusDegrees shouldBe 10.0
-    temperatureQuantityCelsius.toSquants.toCelsiusScale shouldBe 10.0
-    temperatureQuantityCelsius.toSquants.toKelvinDegrees shouldBe 10.0
-    temperatureQuantityCelsius.toSquants.toKelvinScale shouldBe 283.15
+      val temperatureQuantityKelvin = Quantities.getQuantity(100, KELVIN)
 
-    val temperatureQuantityKelvin = Quantities.getQuantity(100, KELVIN)
-
-    temperatureQuantityKelvin.toSquants shouldBe Celsius(-173.15)
-    temperatureQuantityKelvin.toSquants.value shouldBe -173.15
-    temperatureQuantityKelvin.toSquants.toCelsiusDegrees shouldBe -173.15
-    temperatureQuantityKelvin.toSquants.toCelsiusScale shouldBe -173.15
-    temperatureQuantityKelvin.toSquants.toKelvinDegrees shouldBe -173.15
-    (temperatureQuantityKelvin.toSquants.toKelvinScale ~= 100d) shouldBe true
+      temperatureQuantityKelvin.toSquants should approximate(Celsius(-173.15))
+      (temperatureQuantityKelvin.toSquants.value ~= -173.15) shouldBe true
+      (temperatureQuantityKelvin.toSquants.toCelsiusDegrees ~= -173.15) shouldBe true
+      (temperatureQuantityKelvin.toSquants.toCelsiusScale ~= -173.15) shouldBe true
+      (temperatureQuantityKelvin.toSquants.toKelvinDegrees ~= -173.15) shouldBe true
+      (temperatureQuantityKelvin.toSquants.toKelvinScale ~= 100d) shouldBe true
+    }
   }
 }
