48d81cfc7b77d00b315325eda96c6debafe6c244
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-07-22 16:15:23 +0200

    rollback

--- src/main/scala/edu/ie3/simona/model/participant/ParticipantModelShell.scala
+++ src/main/scala/edu/ie3/simona/model/participant/ParticipantModelShell.scala
@@ -176,12 +176,6 @@ final case class ParticipantModelShell[
 
     def modelOperatingPoint(): (OP, OperationChangeIndicator) = {
       val (modelOp, modelNextTick) =
-      if (tick==0 && model.isInstanceOf[StorageModel]) {
-        val (modelOperatingP, changeInd) = model.determineOperatingPoint(currentState,zeroKW)
-
-        (modelOperatingP, changeInd.changesAtTick)
-      }
-      else
         model.determineOperatingPoint(currentState)
       // Sanity check
       if (modelNextTick.exists(_ <= tick))
