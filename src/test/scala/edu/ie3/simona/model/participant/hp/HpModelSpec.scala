dd746fafa9341bd2a915c195a3001540fecf3f09
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2025-07-30 16:31:12 +0200

    Removing more unused imports

--- src/test/scala/edu/ie3/simona/model/participant/hp/HpModelSpec.scala
+++ src/test/scala/edu/ie3/simona/model/participant/hp/HpModelSpec.scala
@@ -18,7 +18,6 @@ import edu.ie3.simona.model.thermal.ThermalGrid.{
 }
 import edu.ie3.simona.model.thermal.ThermalHouse.ThermalHouseState
 import edu.ie3.simona.model.thermal.ThermalHouseTestData
-import edu.ie3.simona.model.thermal.ThermalStorage.ThermalStorageState
 import edu.ie3.simona.test.common.UnitSpec
 import edu.ie3.simona.test.common.input.HpInputTestData
 import edu.ie3.util.scala.quantities.DefaultQuantities.{zeroKW, zeroKWh}
