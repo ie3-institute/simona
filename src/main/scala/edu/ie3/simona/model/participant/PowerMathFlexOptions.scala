8136ae1f058f4b94a5cfdea36d76af153b8b20d4
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2025-08-05 17:20:13 +0200

    Adding const power fo

---
+++ src/main/scala/edu/ie3/simona/model/participant/PowerMathFlexOptions.scala
@@ -0,0 +1,61 @@
+/*
+ * © 2025. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+
+package edu.ie3.simona.model.participant
+
+import edu.ie3.simona.exceptions.CriticalFailureException
+import edu.ie3.simona.model.participant.PowerMathFlexOptions.*
+import edu.ie3.simona.ontology.messages.flex.MathFlexOptions
+import edu.ie3.simona.ontology.messages.flex.MathFlexOptions.OperationVars
+import optimus.algebra.{Const, Expression}
+import optimus.optimization.MPModel
+import squants.Power
+import squants.energy.Kilowatts
+
+import scala.collection.SortedMap
+
+class PowerMathFlexOptions(powers: SortedMap[Long, Power])
+    extends MathFlexOptions[PowerStateVars, PowerOperationVars] {
+
+  override def addInitialState(tick: Long)(using
+      model: MPModel
+  ): PowerStateVars = PowerStateVars(tick)
+
+  override def addOperationConstraints(
+      state: PowerStateVars
+  )(using model: MPModel): PowerOperationVars = {
+    val (_, power) = powers
+      .maxBefore(state.tick + 1)
+      .getOrElse(
+        throw new CriticalFailureException(
+          s"No power found for tick ${state.tick} in provided power set $powers"
+        )
+      )
+    PowerOperationVars(Const(power.toKilowatts))
+  }
+
+  override def addNewStateConstraints(
+      formerState: PowerStateVars,
+      op: PowerOperationVars,
+      tick: Long,
+  )(using model: MPModel): PowerStateVars = PowerStateVars(tick)
+
+}
+
+object PowerMathFlexOptions {
+
+  final case class PowerStateVars(tick: Long)
+
+  final case class PowerOperationVars(power: Const) extends OperationVars {
+
+    override def getPowerExpression: Expression = power
+
+    override def getPowerSolution: Option[Power] = Some(Kilowatts(power.value))
+
+    override def getSoftConstraints: Option[Expression] = None
+  }
+
+}
