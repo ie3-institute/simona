06dce276c835683bdc65b4714e9b4983daa2b876
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2025-09-16 17:02:49 +0200

    Update src/main/scala/edu/ie3/simona/model/em/OptimizedFlexStrat.scala
    Co-authored-by: Daniel Feismann <98817556+danielfeismann@users.noreply.github.com>

--- src/main/scala/edu/ie3/simona/model/em/OptimizedFlexStrat.scala
+++ src/main/scala/edu/ie3/simona/model/em/OptimizedFlexStrat.scala
@@ -30,7 +30,7 @@ import java.util.UUID
   * @param sampleTime
   *   The amount of time in between steps.
   * @param predictionHorizon
-  *   The amount of time that is considered in the future, i.e. the last step is
+  *   The amount of time that is predicted into the future, i.e. the last step is
   *   this amount of time away from now.
   * @param powerObjectiveFactory
   *   The objective to optimize for.
