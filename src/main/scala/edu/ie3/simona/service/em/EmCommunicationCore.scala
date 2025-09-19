38cc896cedb316c54a45a48e729f61e36829785d
staudtMarius <marius.staudt@tu-dortmund.de>
2025-09-19 14:46:17 +0200

    Saving changes.

--- src/main/scala/edu/ie3/simona/service/em/EmCommunicationCore.scala
+++ src/main/scala/edu/ie3/simona/service/em/EmCommunicationCore.scala
@@ -392,7 +392,7 @@ case class EmCommunicationCore(
 
       case control: IssueFlexControl =>
         // send set point to ext
-        log.warn(s"Receiver $receiverUuid got flex request from $sender")
+        log.warn(s"Receiver $receiverUuid got flex control message from $sender")
 
         val (time, power) = control match {
           case IssueNoControl(tick) =>
