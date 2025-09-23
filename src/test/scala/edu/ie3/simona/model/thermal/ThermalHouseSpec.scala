5fa7efd43647ac14c1268414d923fa7443802553
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-09-23 13:13:29 +0200

    optimize imports

--- src/test/scala/edu/ie3/simona/model/thermal/ThermalHouseSpec.scala
+++ src/test/scala/edu/ie3/simona/model/thermal/ThermalHouseSpec.scala
@@ -17,8 +17,8 @@ import edu.ie3.simona.model.thermal.ThermalHouse.{
   startingState,
 }
 import edu.ie3.simona.model.thermal.ThermalStorage.ThermalStorageState
-import edu.ie3.simona.test.common.{DefaultTestData, UnitSpec}
 import edu.ie3.simona.test.common.input.HpInputTestData
+import edu.ie3.simona.test.common.{DefaultTestData, UnitSpec}
 import edu.ie3.simona.util.TickUtil.TickLong
 import edu.ie3.util.scala.quantities.DefaultQuantities.zeroKWh
 import edu.ie3.util.scala.quantities.WattsPerKelvin
