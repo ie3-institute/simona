a8053193c0e2ba61ae7c0028373935c78106828b
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-09-25 12:48:39 +0200

    Revert "simplify method"
    This reverts commit 8355d46e

--- src/main/scala/edu/ie3/simona/model/thermal/ThermalHouse.scala
+++ src/main/scala/edu/ie3/simona/model/thermal/ThermalHouse.scala
@@ -305,7 +305,8 @@ final case class ThermalHouse(
       currentTemperature: Temperature
   ): Energy = {
     val temperatureDiff =
-      (targetTemperature - currentTemperature).max(Kelvin(0))
+      Kelvin(targetTemperature.toKelvinScale - currentTemperature.toKelvinScale)
+        .max(Kelvin(0))
 
     ethCapa * temperatureDiff
   }
