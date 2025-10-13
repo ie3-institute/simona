e8f3b45d926d7b672412911f66ce27abec71a226
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-10-13 11:32:10 +0200

    fmt

--- src/main/scala/edu/ie3/simona/model/thermal/ThermalHouse.scala
+++ src/main/scala/edu/ie3/simona/model/thermal/ThermalHouse.scala
@@ -408,7 +408,7 @@ final case class ThermalHouse(
       qDot.toWatts / ethLosses.toWattsPerKelvin
     ) + Kelvin(thermalHouseState.ambientTemperature.toKelvinScale)
 
-    val maybeNextThreshold = {
+    val maybeNextThreshold =
       if isInnerTemperatureTooLow(limitTemperature + temperatureTolerance) then
       /* Losses and gain of house are not in balance, thus temperature will reach some limit sooner or later */
       /* House has more losses than gain */ {
@@ -434,7 +434,6 @@ final case class ThermalHouse(
         /* House is in perfect balance */
         None
       }
-    }
 
     // If next threshold is negative or zero (e.g. due to overheating beyond target temperature) there should be no next threshold
     maybeNextThreshold match {
