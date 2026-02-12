2da185869ba4fbde6d50443673c4568f22bd6f4d
danielfeismann <daniel.feismann@tu-dortmund.de>
2026-02-12 10:11:00 +0100

    update checkJavaVersion.gradle.kts

--- gradle/scripts/checkJavaVersion.gradle.kts
+++ gradle/scripts/checkJavaVersion.gradle.kts
@@ -1,18 +1,16 @@
 // Enforces the correct Java version, as some parts of the project may malfunction under a wrong version
 // If this task fails, try changing your JAVA_HOME to the required version
-tasks.register("checkJavaVersion") {
-  group = "Verification"
-  description = "Enforces correct Java version"
+tasks.register('checkJavaVersion') {
+  group = 'Verification'
+  description = 'Enforces correct Java version'
 
   doLast {
-    val foundVersion = JavaVersion.current()
-    val requiredVersion = project.extra["javaVersion"] as JavaVersion
-    if (foundVersion != requiredVersion) {
-      throw IllegalStateException("Wrong Java version: required is " +
-        requiredVersion + ", but found " + foundVersion)
+    def foundVersion = JavaVersion.current()
+    if (foundVersion != javaVersion) {
+      throw new IllegalStateException("Wrong Java version: required is $javaVersion, but found $foundVersion")
     }
   }
 }
-tasks.named("compileJava") {
-  dependsOn("checkJavaVersion")
+tasks.named('compileJava') {
+  dependsOn('checkJavaVersion')
 }
