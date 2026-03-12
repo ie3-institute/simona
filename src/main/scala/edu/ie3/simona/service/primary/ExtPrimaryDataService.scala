bdae3a9ad93d48289ce6c9f9bd624e54694cc5b4
staudtMarius <marius.staudt@tu-dortmund.de>
2026-03-12 11:43:40 +0100

    Saving changes.

--- src/main/scala/edu/ie3/simona/service/primary/ExtPrimaryDataService.scala
+++ src/main/scala/edu/ie3/simona/service/primary/ExtPrimaryDataService.scala
@@ -6,24 +6,16 @@
 
 package edu.ie3.simona.service.primary
 
-import edu.ie3.simona.agent.participant.ParticipantAgent
 import edu.ie3.simona.api.data.connection.ExtPrimaryDataConnection
 import edu.ie3.simona.api.ontology.DataMessageFromExt
-import edu.ie3.simona.api.ontology.primary.{
-  PrimaryDataMessageFromExt,
-  ProvidePrimaryData,
-}
+import edu.ie3.simona.api.ontology.primary.{PrimaryDataMessageFromExt, ProvidePrimaryData}
 import edu.ie3.simona.exceptions.WeatherServiceException.InvalidRegistrationRequestException
 import edu.ie3.simona.exceptions.{InitializationException, ServiceException}
 import edu.ie3.simona.ontology.messages.ServiceMessage
 import edu.ie3.simona.ontology.messages.ServiceMessage.*
 import edu.ie3.simona.service.Data.PrimaryData
 import edu.ie3.simona.service.Data.PrimaryData.RichValue
-import edu.ie3.simona.service.ServiceStateData.{
-  InitializeServiceStateData,
-  ServiceBaseStateData,
-}
-import edu.ie3.simona.service.em.ExtEmDataService.Message
+import edu.ie3.simona.service.ServiceStateData.{InitializeServiceStateData, ServiceBaseStateData}
 import edu.ie3.simona.service.{ExtDataSupport, ServiceStateData, SimonaService}
 import org.apache.pekko.actor.typed.ActorRef
 import org.apache.pekko.actor.typed.scaladsl.ActorContext
@@ -31,7 +23,7 @@ import org.slf4j.Logger
 
 import java.util.UUID
 import scala.jdk.CollectionConverters.MapHasAsScala
-import scala.jdk.OptionConverters.RichOptional
+import scala.jdk.OptionConverters.{RichOptional, RichOptionalLong}
 import scala.util.{Failure, Success, Try}
 
 object ExtPrimaryDataService extends SimonaService with ExtDataSupport {
@@ -187,7 +179,7 @@ object ExtPrimaryDataService extends SimonaService with ExtDataSupport {
           }
     }
 
-    val maybeNextTick = primaryDataMessage.maybeNextTick.toScala.map(Long2long)
+    val maybeNextTick = primaryDataMessage.maybeNextTick.toScala
 
     // Distribute Primary Data
     if actorToPrimaryData.nonEmpty then {
--- src/main/scala/edu/ie3/simona/service/primary/ExtPrimaryServiceWorker.scala
+++ src/main/scala/edu/ie3/simona/service/primary/ExtPrimaryServiceWorker.scala
@@ -6,23 +6,16 @@
 
 package edu.ie3.simona.service.primary
 
-import edu.ie3.simona.agent.participant.ParticipantAgent
 import edu.ie3.simona.api.data.connection.ExtPrimaryDataConnection
 import edu.ie3.simona.api.ontology.DataMessageFromExt
-import edu.ie3.simona.api.ontology.primary.{
-  PrimaryDataMessageFromExt,
-  ProvidePrimaryData,
-}
+import edu.ie3.simona.api.ontology.primary.{PrimaryDataMessageFromExt, ProvidePrimaryData}
 import edu.ie3.simona.exceptions.WeatherServiceException.InvalidRegistrationRequestException
 import edu.ie3.simona.exceptions.{InitializationException, ServiceException}
 import edu.ie3.simona.ontology.messages.ServiceMessage
 import edu.ie3.simona.ontology.messages.ServiceMessage.*
 import edu.ie3.simona.service.Data.PrimaryData
 import edu.ie3.simona.service.Data.PrimaryData.RichValue
-import edu.ie3.simona.service.ServiceStateData.{
-  InitializeServiceStateData,
-  ServiceBaseStateData,
-}
+import edu.ie3.simona.service.ServiceStateData.{InitializeServiceStateData, ServiceBaseStateData}
 import edu.ie3.simona.service.{ExtDataSupport, ServiceStateData, SimonaService}
 import org.apache.pekko.actor.typed.ActorRef
 import org.apache.pekko.actor.typed.scaladsl.ActorContext
@@ -30,7 +23,7 @@ import org.slf4j.Logger
 
 import java.util.UUID
 import scala.jdk.CollectionConverters.MapHasAsScala
-import scala.jdk.OptionConverters.RichOptional
+import scala.jdk.OptionConverters.{RichOptional, RichOptionalLong}
 import scala.util.{Failure, Success, Try}
 
 object ExtPrimaryServiceWorker extends SimonaService with ExtDataSupport {
@@ -193,7 +186,7 @@ object ExtPrimaryServiceWorker extends SimonaService with ExtDataSupport {
     )
 
     val uuidToAgent = serviceStateData.uuidToActorRef
-    val maybeNextTick = primaryDataMessage.maybeNextTick.toScala.map(Long2long)
+    val maybeNextTick = primaryDataMessage.maybeNextTick.toScala
 
     primaryDataMessage.primaryData.asScala.foreach { case (agentUuid, data) =>
       data.toPrimaryData match {
