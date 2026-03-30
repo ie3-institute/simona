4e356b4c78f73a9234dae33eeb7079ed0e680c9d
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2026-03-30 20:24:40 +0200

    Improving comment text

--- src/main/scala/edu/ie3/simona/model/em/opt/CommonLossObjectiveFactory.scala
+++ src/main/scala/edu/ie3/simona/model/em/opt/CommonLossObjectiveFactory.scala
@@ -247,8 +247,8 @@ object CommonLossObjectiveFactory {
         val absPower = maxTotalPower.max(-minTotalPower).toKilowatts
         if absPower == 0.0 then {
           // if there is zero maximum absolute power, the only solution is zero power
-          // for all devices. We thus just provide assume a placeholder value here so
-          // that numerics do not break
+          // for all assets. We thus just assume a placeholder value here so that 
+          // numerics do not break
           1.0
         } else absPower
       }
