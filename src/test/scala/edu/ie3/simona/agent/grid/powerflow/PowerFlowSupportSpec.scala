e930739f982009fe62de0821db4ffa25622eeb40
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2026-03-31 17:30:52 +0200

    Reinstated one import

--- src/test/scala/edu/ie3/simona/agent/grid/powerflow/PowerFlowSupportSpec.scala
+++ src/test/scala/edu/ie3/simona/agent/grid/powerflow/PowerFlowSupportSpec.scala
@@ -54,6 +54,7 @@ import tech.units.indriya.ComparableQuantity
 import java.time.ZonedDateTime
 import java.util.UUID
 import javax.measure.quantity.Angle
+import scala.jdk.CollectionConverters.SetHasAsJava
 import scala.language.implicitConversions
 
 /** Tests power flow on a grid with switches on two branches, where depending on
