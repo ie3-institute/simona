b91c7ce68be556d239ad9b3096ef3635e4670e13
danielfeismann <daniel.feismann@tu-dortmund.de>
2026-05-08 11:44:22 +0200

    Revert "update copyright"
    This reverts commit 2b670956e81e358e6e1a6f4816c5412f5b4adf53.

--- src/main/scala/edu/ie3/simona/sim/setup/ExtSimSetupData.scala
+++ src/main/scala/edu/ie3/simona/sim/setup/ExtSimSetupData.scala
@@ -1,5 +1,5 @@
 /*
- * © 2021-2026. TU Dortmund University,
+ * © 2021. TU Dortmund University,
  * Institute of Energy Systems, Energy Efficiency and Energy Economics,
  * Research group Distribution grid planning and operation
  */
