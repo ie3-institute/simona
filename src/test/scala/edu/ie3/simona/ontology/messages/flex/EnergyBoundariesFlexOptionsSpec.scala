f030824ccbf8cb9024348301c84d2b87f822ecb2
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2026-06-01 21:35:48 +0200

    Improving tests

--- src/test/scala/edu/ie3/simona/ontology/messages/flex/EnergyBoundariesFlexOptionsSpec.scala
+++ src/test/scala/edu/ie3/simona/ontology/messages/flex/EnergyBoundariesFlexOptionsSpec.scala
@@ -17,7 +17,7 @@ import edu.ie3.simona.ontology.messages.flex.FlexibilityMessage.{
 import edu.ie3.simona.test.common.{DefaultTestData, UnitSpec}
 import edu.ie3.util.interval.ClosedInterval
 import edu.ie3.util.quantities.QuantityUtils.{asMegaWatt, asMegaWattHour}
-import edu.ie3.util.scala.quantities.DefaultQuantities.{onePU, zeroKW, zeroKWh}
+import edu.ie3.util.scala.quantities.DefaultQuantities.{onePU, zeroKW}
 import squants.{Each, Energy}
 import squants.energy.{KilowattHours, Kilowatts}
 import squants.time.Hours
@@ -99,10 +99,16 @@ class EnergyBoundariesFlexOptionsSpec extends UnitSpec with DefaultTestData {
 
   "Tightening energy boundaries should result in proper energy limits" when {
 
-    val startTick = 3600L
-    val sampleTicks = 1800L
     val sampleTime = Hours(0.5)
     val predictionHorizon = Hours(4)
+
+    val ticks = {
+      val startTick = 3600L
+      val sampleTicks = sampleTime.toSeconds.toLong
+      val lastPredictedTick = startTick + predictionHorizon.toSeconds.toLong
+
+      Range.Long.inclusive(startTick, lastPredictedTick, sampleTicks)
+    }
 
     "providing typical battery storage options with high capacity" in {
 
@@ -113,22 +119,22 @@ class EnergyBoundariesFlexOptionsSpec extends UnitSpec with DefaultTestData {
           pMax = Kilowatts(10),
           etaCharge = Each(0.8),
           etaDischarge = Each(0.8),
-          currentTick = startTick,
+          currentTick = ticks(0),
         ),
         sampleTime = sampleTime,
         predictionHorizon = predictionHorizon,
       )
 
       adaptedBoundaries.energyLimits shouldBe SortedMap(
-        startTick + 0 * sampleTicks -> limits(20, 20),
-        startTick + 1 * sampleTicks -> limits(13.75, 24),
-        startTick + 2 * sampleTicks -> limits(7.5, 28),
-        startTick + 3 * sampleTicks -> limits(1.25, 32),
-        startTick + 4 * sampleTicks -> limits(0, 36),
-        startTick + 5 * sampleTicks -> limits(0, 40),
-        startTick + 6 * sampleTicks -> limits(0, 44),
-        startTick + 7 * sampleTicks -> limits(0, 48),
-        startTick + 8 * sampleTicks -> limits(0, 50),
+        ticks(0) -> limits(20, 20),
+        ticks(1) -> limits(13.75, 24),
+        ticks(2) -> limits(7.5, 28),
+        ticks(3) -> limits(1.25, 32),
+        ticks(4) -> limits(0, 36),
+        ticks(5) -> limits(0, 40),
+        ticks(6) -> limits(0, 44),
+        ticks(7) -> limits(0, 48),
+        ticks(8) -> limits(0, 50),
       )
 
     }
@@ -142,22 +148,22 @@ class EnergyBoundariesFlexOptionsSpec extends UnitSpec with DefaultTestData {
           pMax = Kilowatts(10),
           etaCharge = Each(0.8),
           etaDischarge = Each(0.8),
-          currentTick = startTick,
+          currentTick = ticks(0),
         ),
         sampleTime = sampleTime,
         predictionHorizon = predictionHorizon,
       )
 
       adaptedBoundaries.energyLimits shouldBe SortedMap(
-        startTick + 0 * sampleTicks -> limits(4, 4),
-        startTick + 1 * sampleTicks -> limits(0, 8),
-        startTick + 2 * sampleTicks -> limits(0, 8),
-        startTick + 3 * sampleTicks -> limits(0, 8),
-        startTick + 4 * sampleTicks -> limits(0, 8),
-        startTick + 5 * sampleTicks -> limits(0, 8),
-        startTick + 6 * sampleTicks -> limits(0, 8),
-        startTick + 7 * sampleTicks -> limits(0, 8),
-        startTick + 8 * sampleTicks -> limits(0, 8),
+        ticks(0) -> limits(4, 4),
+        ticks(1) -> limits(0, 8),
+        ticks(2) -> limits(0, 8),
+        ticks(3) -> limits(0, 8),
+        ticks(4) -> limits(0, 8),
+        ticks(5) -> limits(0, 8),
+        ticks(6) -> limits(0, 8),
+        ticks(7) -> limits(0, 8),
+        ticks(8) -> limits(0, 8),
       )
 
     }
@@ -168,14 +174,8 @@ class EnergyBoundariesFlexOptionsSpec extends UnitSpec with DefaultTestData {
         boundaries = AssetEnergyBoundaries(
           currentEnergy = KilowattHours(10),
           energyLimits = SortedMap(
-            startTick -> new ClosedInterval(
-              zeroKWh,
-              KilowattHours(50),
-            ),
-            18000L -> new ClosedInterval(
-              KilowattHours(40),
-              KilowattHours(50),
-            ),
+            ticks(0) -> limits(0, 50),
+            ticks(8) -> limits(40, 50),
           ),
           powerLimits = ClosedInterval(zeroKW, Kilowatts(10)),
           tickDisconnect = Some(18000L),
@@ -185,15 +185,54 @@ class EnergyBoundariesFlexOptionsSpec extends UnitSpec with DefaultTestData {
       )
 
       adaptedBoundaries.energyLimits shouldBe SortedMap(
-        startTick + 0 * sampleTicks -> limits(10, 10),
-        startTick + 1 * sampleTicks -> limits(10, 15),
-        startTick + 2 * sampleTicks -> limits(10, 20),
-        startTick + 3 * sampleTicks -> limits(15, 25),
-        startTick + 4 * sampleTicks -> limits(20, 30),
-        startTick + 5 * sampleTicks -> limits(25, 35),
-        startTick + 6 * sampleTicks -> limits(30, 40),
-        startTick + 7 * sampleTicks -> limits(35, 45),
-        startTick + 8 * sampleTicks -> limits(40, 50),
+        ticks(0) -> limits(10, 10),
+        ticks(1) -> limits(10, 15),
+        ticks(2) -> limits(10, 20),
+        ticks(3) -> limits(15, 25),
+        ticks(4) -> limits(20, 30),
+        ticks(5) -> limits(25, 35),
+        ticks(6) -> limits(30, 40),
+        ticks(7) -> limits(35, 45),
+        ticks(8) -> limits(40, 50),
+      )
+
+    }
+
+    "providing challenging energy limits" in {
+
+      val adaptedBoundaries = AssetEnergyBoundaries.tighten(
+        boundaries = AssetEnergyBoundaries(
+          currentEnergy = KilowattHours(25),
+          energyLimits = SortedMap(
+            ticks(0) -> limits(0, 50),
+            ticks(1) -> limits(10, 40),
+            ticks(2) -> limits(25, 25),
+            ticks(3) -> limits(10, 40),
+            ticks(4) -> limits(10, 40),
+            ticks(5) -> limits(0, 50),
+            ticks(6) -> limits(25, 25),
+            ticks(7) -> limits(0, 20),
+            ticks(8) -> limits(25, 50),
+          ),
+          powerLimits = ClosedInterval(Kilowatts(-6), Kilowatts(16)),
+          etaCharge = Each(0.625),
+          etaDischarge = Each(0.6),
+          tickDisconnect = Some(ticks(8)),
+        ),
+        sampleTime = sampleTime,
+        predictionHorizon = predictionHorizon,
+      )
+
+      adaptedBoundaries.energyLimits shouldBe SortedMap(
+        ticks(0) -> limits(25, 25),
+        ticks(1) -> limits(20, 30),
+        ticks(2) -> limits(25, 25),
+        ticks(3) -> limits(20, 30),
+        ticks(4) -> limits(15, 35),
+        ticks(5) -> limits(20, 30),
+        ticks(6) -> limits(25, 25),
+        ticks(7) -> limits(20, 20),
+        ticks(8) -> limits(25, 25),
       )
 
     }
@@ -204,6 +243,8 @@ class EnergyBoundariesFlexOptionsSpec extends UnitSpec with DefaultTestData {
 
 object EnergyBoundariesFlexOptionsSpec {
 
+  /** Helper method to create energy limits.
+    */
   private def limits(lower: Double, upper: Double): ClosedInterval[Energy] =
     new ClosedInterval(KilowattHours(lower), KilowattHours(upper))
 
