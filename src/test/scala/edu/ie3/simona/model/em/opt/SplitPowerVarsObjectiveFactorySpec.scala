70164fce95f1c718328209930523f7185d7b9359
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2026-06-03 19:34:34 +0200

    Implementation

---
+++ src/test/scala/edu/ie3/simona/model/em/opt/SplitPowerVarsObjectiveFactorySpec.scala
@@ -0,0 +1,15 @@
+/*
+ * © 2026. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
+
+package edu.ie3.simona.model.em.opt
+
+import edu.ie3.simona.test.common.UnitSpec
+
+class SplitPowerVarsObjectiveFactorySpec extends UnitSpec {
+
+  // todo
+
+}
