7a5e6e13c3eacdb537287e7f4ac2c77fefd8cf93
Sebastian Peter <sebastian.peter@tu-dortmund.de>
2026-06-16 12:17:04 +0200

    Making test single-solution only

--- src/test/scala/edu/ie3/simona/model/em/opt/CommonLossObjectiveFactorySpec.scala
+++ src/test/scala/edu/ie3/simona/model/em/opt/CommonLossObjectiveFactorySpec.scala
@@ -605,7 +605,7 @@ class CommonLossObjectiveFactorySpec
       val batFlex = EnergyBoundariesFlexOptions(
         AssetEnergyBoundaries(
           eStorage = KilowattHours(12),
-          currentEnergy = KilowattHours(6),
+          currentEnergy = KilowattHours(5),
           pMax = Kilowatts(10),
           etaCharge = Each(0.8),
           etaDischarge = Each(0.8),
@@ -689,24 +689,24 @@ class CommonLossObjectiveFactorySpec
           evcsRes(0).energyVal should approximate(7)
           // battery is left with 0
           batRes(0).pVal should approximate(0)
-          batRes(0).energyVal should approximate(6)
+          batRes(0).energyVal should approximate(5)
 
           // EV needs to take the 4 kW to reach its target
           evcsRes(1).pVal should approximate(4)
           evcsRes(1).energyVal should approximate(9)
           // battery is left with 0
           batRes(1).pVal should approximate(0)
-          batRes(1).energyVal should approximate(6)
+          batRes(1).energyVal should approximate(5)
 
           // EV is not available from here on
 
           // discharging 5 kWh
           batRes(2).pVal should approximate(-8)
-          batRes(2).energyVal should approximate(1)
+          batRes(2).energyVal should approximate(0)
 
           // charging 3.2 kWh
           batRes(3).pVal should approximate(8)
-          batRes(3).energyVal should approximate(4.2)
+          batRes(3).energyVal should approximate(3.2)
 
         } withClue buildDebugString(assetSymbols)
 
