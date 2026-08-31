8197cee659478f08510527e6d78a57c670622442
Pierre <155652256+pierrepetersmeier@users.noreply.github.com>
2026-08-31 18:15:04 +0200

    Override lazy val declarations in CongestedComponentsTestData

--- src/test/scala/edu/ie3/simona/test/common/result/CongestedComponentsTestData.scala
+++ src/test/scala/edu/ie3/simona/test/common/result/CongestedComponentsTestData.scala
@@ -31,7 +31,7 @@ import java.util.UUID
 
 trait CongestedComponentsTestData extends NodeInputTestData with DbfsTestGrid {
 
-  protected lazy val typesafeConfig = ConfigFactory
+  override protected lazy val typesafeConfig = ConfigFactory
     .parseString(
       """
       |simona.simulationName = "CongestedComponentsTest"
@@ -52,9 +52,9 @@ trait CongestedComponentsTestData extends NodeInputTestData with DbfsTestGrid {
     )
     .resolve()
 
-  protected lazy val simonaConfig: SimonaConfig = SimonaConfig(typesafeConfig)
-  protected lazy val startTime: ZonedDateTime = simonaConfig.time.simStartTime
-  protected lazy val endTime: ZonedDateTime = startTime.plusHours(2)
+  override protected lazy val simonaConfig: SimonaConfig = SimonaConfig(typesafeConfig)
+  override protected lazy val startTime: ZonedDateTime = simonaConfig.time.simStartTime
+  override protected lazy val endTime: ZonedDateTime = startTime.plusHours(2)
 
   val trafoType3W = new Transformer3WTypeInput(
     UUID.randomUUID(),
